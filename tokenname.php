a6773e982d91dcb7c6d9b432a1e0e47fb14f494d
Seguy Damien <alterway@AlterWayConsulting.local>
2010-03-26 08:58:27 +0100

    mise sous git

---
+++ tokenname.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+
+$args = $argv;
+array_shift($args);
+
+while($arg = array_shift($args)) {
+    if (!is_numeric($arg)) {
+        if (defined($arg)) {
+            print "$arg ".constant($arg)."\n";
+        } else {
+            print "$arg UNKNOWN\n";
+        }
+    } else {
+        print "$arg ".token_name($arg)."\n";
+    }
+}
+
+?>
