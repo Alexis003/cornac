ad4da075c50d79e9b669fec56d9b0754fbc5be00
Seguy Damien <alterway@AlterWayConsulting.local>
2010-05-21 16:30:54 +0200

    correction de la detection des creations de constantes

--- auditeur/classes/defconstantes.php
+++ auditeur/classes/defconstantes.php
@@ -16,15 +16,16 @@ class defconstantes extends modules {
 
 	    $requete = <<<SQL
 INSERT INTO <rapport>
-    SELECT 0, tokens.fichier, T3.code, tokens.id, '{$this->name}'
+SELECT 0, T1.fichier, T3.code, T3.id, '{$this->name}'
     FROM <tokens> T1
-    JOIN tokens_tags
-      ON T1.id = TT.token_id AND TT.type = 'fonction'
     JOIN <tokens> T2
-      ON TT.token_sub_id = T2.id AND T2.fichier = T1.fichier
+    ON T1.droite + 1 = T2.droite
+       AND T1.fichier=  T2.fichier
     JOIN <tokens> T3
-      ON T3.droite = T2.droite + 3 AND T1.fichier = T3.fichier
-    WHERE T1.type = 'functioncall' AND T2.code='define'
+    ON T1.droite + 4 = T3.droite
+       AND T1.fichier=  T3.fichier
+    WHERE T1.type='functioncall' AND
+          T2.code = 'define';
 SQL;
         $this->exec_query($requete);
 
