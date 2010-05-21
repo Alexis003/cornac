503e153b8b5c4f63117756b9592a86d5b1425f07
Seguy Damien <alterway@AlterWayConsulting.local>
2010-05-21 17:03:31 +0200

    suppression du cache ancienne mode

--- auditeur/classes/parentheses.php
+++ auditeur/classes/parentheses.php
@@ -14,32 +14,14 @@ class parentheses extends modules {
         $this->clean_rapport();
 
         $requete = <<<SQL
-INSERT INTO caches
-   SELECT T1.fichier, T1.id, 'parentheses',T2.code 
+INSERT INTO <rapport>
+   SELECT 0, T1.fichier, T2.code,  T1.id, 'parentheses'
    FROM <tokens> T1
    JOIN <tokens_cache> T2
    ON T1.id = T2.id
    WHERE T1.type = 'parentheses';
 SQL;
-    print $this->prepare_query($requete);
-    die();
-        $res = $this->exec_query($requete);
-
-        $requete = <<<SQL
-SELECT fichier, droite, gauche FROM <tokens> WHERE type = 'parentheses';
-SQL;
-        $res = $this->exec_query($requete);
-        $this->functions = array();
-        include_once('classes/rendu.php');
-        $rendu = new rendu($this->mid);
-
-        while($ligne = $res->fetch(PDO::FETCH_ASSOC)) {
-            $code = $rendu->rendu($ligne['droite'] + 1, $ligne['gauche'] - 1, $ligne['fichier']);
-
-            $this->functions[$ligne['fichier']][$code] = 1;
-            $this->occurrences++;
-        }
-        $this->fichiers_identifies = count($this->functions);
+    $this->exec_query($requete);
     }
 }
 
