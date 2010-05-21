d5ef01f1797d183e342834e362e17a5d3b889479
Seguy Damien <alterway@AlterWayConsulting.local>
2010-05-21 16:57:47 +0200

    correction fonctionnelle

--- auditeur/classes/emptyfunctions.php
+++ auditeur/classes/emptyfunctions.php
@@ -15,19 +15,25 @@ class emptyfunctions extends modules {
 
 	    $requete = <<<SQL
 INSERT INTO <rapport>
-    SELECT 0, T1.fichier, group_concat(if(T2.droite = T1.droite + 1, T2.code, '') SEPARATOR '') , T2.id, '{$this->name}'
+    SELECT 0, T1.fichier, T4.code, T1.id, '{$this->name}'
     FROM <tokens> T1 
-    JOIN <tokens> T2 
-        ON T2.droite > T1.droite AND
-           T2.gauche < T1.gauche
+    JOIN <tokens_tags> T2
+        ON T1.id = T2.token_id
+    JOIN <tokens> T3
+    ON T3.id = T2.token_sub_id
+    JOIN <tokens_tags> T5
+        ON T1.id = T5.token_id AND T5.type = 'name'
+    JOIN <tokens> T4
+    ON T1.fichier = T4.fichier AND
+       T4.id = T5.token_sub_id
     WHERE 
-        T1.fichier='./tests.php' AND
-        T1.type = '_function' AND 
-        T2.fichier = './tests.php' 
-        GROUP BY T1.id
-        HAVING empty_bloc = 'vide'
+        T1.type = '_function' AND
+        T2.type = 'block' AND
+        T3.gauche - T3.droite = 1
+        ;
 SQL;
-        $this->exec_query($requete);
+
+    $this->exec_query($requete);
 	}
 }
 
