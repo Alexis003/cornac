1c00b311ae3a7d51003a299094612c95d8fcfbb3
Seguy Damien <alterway@AlterWayConsulting.local>
2010-06-10 14:52:37 +0200

    extraction de caceis

--- auditeur/classes/zfController.php
+++ auditeur/classes/zfController.php
@@ -29,37 +29,6 @@ ORDER BY T2.class, T2.code;
 SQL;
     $this->exec_query($requete);
 	}
-	
-	/*
-	
-	Extrait les fonctions qui ont du getParam mais pas de isValid.
-SELECT CR.fichier, T1.class, T1.scope, 
-    sum(if (CR.element in ('getParams','getParam'), 1, 0)) as getParams, 
-    sum(if (CR.element in ('isValid','isErrors'), 1, 0)) as isValid
-FROM caceis_rapport CR
-JOIN caceis T1
-ON CR.token_id = T1.id
- WHERE module='zfGetGPC'
-GROUP BY class, scope
-HAVING getParams > 0 and  isValid = 0
-ORDER BY class, scope, element
- ;
- 
- Extrait les actions des controleurs
- 
- SELECT T2.class, T2.code
-FROM caceis T1
-JOIN caceis_tags TT 
-ON T1.id = TT.token_id AND
-   TT.type='name'
-JOIN caceis T2
-ON T2.id = TT.token_sub_id AND
-   T2.fichier=T1.fichier
-WHERE T1.type = '_function' AND
-T2.code like "%action"
-ORDER BY class, code;
-
-	*/
 }
 
 ?>
