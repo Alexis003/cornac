a80c23ca97bc72c7f5d93200c1f31ba2248d6fb3
Seguy Damien <alterway@alterwayconsulting.ingenidev>
2010-06-16 16:21:38 +0200

    support de T_ELSEIF

--- prepare/arginit_literal_regex.php
+++ prepare/arginit_literal_regex.php
@@ -19,12 +19,12 @@ class arginit_literal_regex extends analyseur_regex {
         if ($t->checkNotClass(array('variable','constante','reference'))) { return false; }
         if ($t->getNext()->checkNotCode('=')) { return false; }
         if ($t->getNext(1)->checkNotClass(array('constante','literals','signe'))) { return false; }
-        if ($t->getPrev(1)->checkToken(array(T_FOR))) { return false; }
+        if ($t->getPrev(1)->checkToken(array(T_FOR,T_IF, T_ELSEIF))) { return false; }
         if ($t->getNext(2)->checkNotCode(array(',',')')))  { return false;}
 
         $this->args = array(0, 2);
         $this->remove = array(1, 2);
-
+        
         mon_log(get_class($t)." => ".__CLASS__);
         return true; 
     }
