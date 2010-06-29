146d44c0b48f392eff7bb4f3b1dfa373e2f366ec
Seguy Damien <alterway@AlterWayConsulting.local>
2010-06-29 16:43:05 +0200

    look for html tags in the programm

---
+++ auditeur/classes/html_tags.php
@@ -0,0 +1,29 @@
+<?php
+
+class html_tags extends noms {
+	protected	$description = 'Liste des literaux ayant du code HTML';
+	protected	$description_en = 'List of HTML tags in literals';
+
+	function __construct($mid) {
+        parent::__construct($mid);
+        
+    	$this->name = __CLASS__;
+	}
+	
+	public function analyse() {
+        $this->clean_rapport();
+
+        $requete = <<<SQL
+INSERT INTO <rapport>
+    SELECT NULL, T1.fichier, T1.code AS code, T1.id, '{$this->name}'
+    FROM <tokens> T1
+    WHERE type='literals' AND code like "%<%>%"
+SQL;
+        $this->exec_query($requete);
+        
+        // liste des tags!DOCTYPE|a|abbr|acronym|address|applet|area|b|base|basefont|bdo|big|blockquote|body|br|button|caption|center|cite|code|col|colgroup|dd|del|dfn|dir|div|dl|dt|em|fieldset|font|form|frame|frameset|h1|head|hr|html|i|iframe|img|input|ins|isindex|kbd|label|legend|li|link|map|menu|meta|noframes|noscript|object|ol|optgroup|option|p|param|pre|q|s|samp|script|select|small|span|strike|strong|style|sub|sup|table|tbody|td|textarea|tfoot|th|thead|title|tr|tt|u|ul|var|xmp|
+        return;
+	}
+}
+
+?>
