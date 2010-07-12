90fb05ad9512518b08be6fc7c317436cbada949b
Seguy Damien <alterway@alterwayconsulting.ingenidev>
2010-07-12 16:33:58 +0200

    correct option handling in cornac.php

--- cornac.php
+++ cornac.php
@@ -4,51 +4,92 @@
 include('libs/getopts.php');
 $args = $argv;
 
-$help = get_arg($args, '-?') ;
-if ($help) { help(); }
+if (get_arg($args, '-?')) { help(); }
 
-$origin = get_arg_value($args, '-d', null);
-if (is_null($origin)) { help(); }
+// default values, stored in a INI file
+$ini = get_arg_value($args, '-I', null);
+if (!is_null($ini)) {
+    global $INI;
+    if (file_exists('ini/'.$ini)) {
+        define('INI','ini/'.$ini);
+    } elseif (file_exists('ini/'.$ini.".ini")) {
+        define('INI','ini/'.$ini.".ini");
+    } elseif (file_exists($ini)) {
+        define('INI',$ini);
+    } else {
+        define('INI','ini/'.'tokenizeur.ini');
+    }
+    $INI = parse_ini_file(INI, true);
+} else {
+    define('INI',null);
+    $INI = array('cornac' => array('destination' => ''));
+}
+$INI['cornac']['ini'] = $ini;
+unset($ini);
 
-$destination = get_arg_value($args, '-o', null);
-if (is_null($destination)) { help(); }
+$INI['cornac']['origin'] = get_arg_value($args, '-d', null);
+if (is_null($INI['cornac']['origin'])) { 
+    print "Origin folder is missing : option -d\n";
+    help(); 
+}
 
-if (!file_exists($origin)) {
-    print "Source folder '$origin' doesn't exist\n";
+$INI['cornac']['destination'] = get_arg_value($args, '-o', @$INI['cornac']['destination']);
+if (empty($INI['cornac']['destination'])) { 
+    print "Destination folder is missing : option -o\n";
+    help(); 
+}
+
+$INI['cornac']['storage'] = get_arg_value($args, '-s', @$INI['cornac']['storage'] ?: 'mysql' );
+if (!in_array($INI['cornac']['storage'],array('mysql','sqlite'))) { 
+    print "No storage provided : option -s\n";
+    help(); 
+}
+
+
+// validations
+if (!file_exists($INI['cornac']['origin'])) {
+    print "Source folder '{$INI['cornac']['origin']}' doesn't exist\n";
     die();
 }
 
-if (!file_exists($destination)) {
-    print "Output folder '$destination' doesn't exist\n";
+if (!file_exists($INI['cornac']['destination'])) {
+    print "Output folder '{$INI['cornac']['destination']}' doesn't exist\n";
     die();
 }
 
-if (realpath($origin) == realpath($destination)) {
+if (realpath($INI['cornac']['origin']) == realpath($INI['cornac']['destination'])) {
     print "Please, don't use the same folder for source and destination\n";
     die();
 }
 
-print "Dossier : $origin 
-Output : $destination\n";
+// execution
+print "
+Folder : {$INI['cornac']['origin']} 
+Output : {$INI['cornac']['destination']}\n";
 
-shell_exec("./tokenizeur.php -r -d $origin -g mysql,cache > /dev/null"); // @todo : note the log 
-                                                                         // @sqlite as default ? 
-shell_exec("cd auditeur; ./auditeur.php -p tokens -o html");
+if (!empty($INI['cornac']['ini'])) { $ini = " -I {$INI['cornac']['ini']} "; } else { $ini = ""; }
 
-shell_exec("rm -rf /tmp/cornac; mkdir /tmp/cornac");
-
-shell_exec("cd auditeur; ./lecture_module.php -p tokens -F html -o /tmp/cornac ");
+print shell_exec("./tokenizeur.php -r -d {$INI['cornac']['origin']} -g {$INI['cornac']['storage']},cache $ini "); // @todo : note the log 
+                                                                                        // @sqlite as default ? 
+print shell_exec("cd auditeur; ./auditeur.php $ini -o -d {$INI['cornac']['destination']}");
+// @todo clean audits tables before. shell_exec("rm -rf /tmp/cornac; mkdir {$INI['destination']}");
+print shell_exec("cd auditeur; ./reader.php $ini -F html -o {$INI['cornac']['destination']} ");
 
 print "Done\n";
 
+
 function help() {
     print <<<SHELL
+
 Usage : ./cornac -d <source folder> -o <output folder>
 
 Options : 
   -? : help
   -d : source folder
-  -o : output folder
+  -s : storage (mysql (default), sqlite)
+  -o : output folder (path, 'web' (leave in database'))
+  -I : ini config
+
 SHELL;
     die();
 }
