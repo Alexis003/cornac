4a9b481b7dc450aee0e9e4b8dadb4067671ca26d
Seguy Damien <alterway@alterwayconsulting.ingenidev>
2010-07-12 16:32:52 +0200

    synchronisation of Options reading and handling

--- auditeur/auditeur.php
+++ auditeur/auditeur.php
@@ -1,43 +1,16 @@
 #!/usr/bin/php
 <?php
 
-if ($id = array_search('-?', $argv)) {
-    help();
-    die();
-}
-
-if ($id = array_search('-h', $argv)) {
-    help();
-    die();
-}
-
-if ($id = array_search('-help', $argv)) {
-    help();
-    die();
-}
-
-if ($id = array_search('-d', $argv)) {
-    define("DO_DEPENDENCES",true);
-    unset($args[$id]);
-} else {
-    define("DO_DEPENDENCES",false);
-}
+include('../libs/getopts.php');
 
 $args = $argv;
-if ($id = array_search('-p', $argv)) {
-    $prefixe = $args[$id + 1];
-    unset($args[$id]);
-    unset($args[$id + 1]);
-} else {
-    $prefixe = 'tokens';
-}
 
-if ($id = array_search( '-I', $argv)) {
-    $ini = $argv[$id + 1];
-    
-    unset($argv[$id]);
-    unset($argv[$id + 1]);
-    
+$help = get_arg($args, '-?') ;
+if ($help) { help(); }
+
+// default values, stored in a INI file
+$ini = get_arg_value($args, '-I', null);
+if (!is_null($ini)) {
     global $INI;
     if (file_exists('../ini/'.$ini)) {
         define('INI','../ini/'.$ini);
@@ -48,19 +21,20 @@ if ($id = array_search( '-I', $argv)) {
     } else {
         define('INI','../ini/'.'tokenizeur.ini');
     }
+    $INI = parse_ini_file(INI, true);
 } else {
-    define('INI','../ini/'.'tokenizeur.ini');
+    define('INI',null);
+    $INI = array();
 }
+unset($ini);
 
-// @todo : que faire si on ne trouve même pas le .ini ? 
-print "Fichier de directives : ".INI."\n";
-global $INI;
-$INI = parse_ini_file(INI, true);
+$INI['dependences'] = (bool) get_arg_value($args, '-d', false);
+
 
 $modules = array(
 '_new',
 'affectations_variables',
-//'appelsfonctions',
+//'appelsfonctions', // @_
 'arobases',
 'classes',
 'classes_hierarchie',
@@ -136,68 +110,109 @@ $modules = array(
 'classes_unused',
 'classes_undefined',
 'html_tags', 
-'affectations_gpc', 
+//'affectations_gpc', 
+'classes_nb_methods',
 
 );
 
-if ($id = array_search('-a', $argv)) {
+$INI['analyzers'] = get_arg_value($args, '-a', 'all');
+if ($INI['analyzers'] == 'all' ) {
+ // default : all modules
+} else {
     $m = explode(',', $args[$id + 1]);
-    unset($args[$id]);
-    unset($args[$id + 1]);
 
     $diff = array_diff($m , $modules);
     if (count($diff) > 0) {
-        print count($diff)." modules are unknown, and omitted : ".join(', ', $diff)."\n";
+        print count($diff)." analyzers are unknown and omitted : ".join(', ', $diff)."\n";
     }
     
-    $m = array_intersect($m, $modules);
-    
+    $m = array_intersect($m, $modules);    
     if (count($m) == 0) {
         print "No analyzer provided : aborting\n";
         die();
     } else {
         $modules = $m;
     }
-} else {
-    // rien 
-}
-
+} 
 print count($modules)." modules will be treated : ".join(', ', $modules)."\n";
-
-
-print "Work with prefixes '$prefixe'\n";
-
 
 if (isset($INI['mysql']) && $INI['mysql']['active'] == true) {
     $database = new pdo($INI['mysql']['dsn'],$INI['mysql']['username'], $INI['mysql']['password']);
+
+//    $database->query('DELETE FROM '.$INI['template.mysql'].'_rapport WHERE fichier = "'.$fichier.'"');
+    $database->query('CREATE TABLE IF NOT EXISTS '.$INI['template.mysql'].'_rapport (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fichier` varchar(500) NOT NULL,
+  `element` varchar(500) NOT NULL,
+  `token_id` int(10) unsigned NOT NULL,
+  `module` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `element` (`element`),
+  KEY `module` (`module`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1');
+
+//        $database->query('DELETE FROM '.$INI['template.mysql']['table'].'_rapport_dot WHERE fichier = "'.$fichier.'"');
+        $database->query('CREATE TABLE IF NOT EXISTS '.$INI['template.mysql']['table'].'_rapport_dot (
+  `a` varchar(255) NOT NULL,
+  `b` varchar(255) NOT NULL,
+  `cluster` varchar(255) NOT NULL DEFAULT \'\',
+  `module` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1');
+
+//        $database->query('DELETE FROM '.$INI['template.mysql']['table'].'_rapport_module WHERE fichier = "'.$fichier.'"');
+        $database->query('CREATE TABLE IF NOT EXISTS '.$INI['template.mysql']['table'].'_rapport_module (
+  `module` varchar(255) NOT NULL,
+  `fait` datetime NOT NULL,
+  `format` enum("html","dot","gefx") NOT NULL,
+  PRIMARY KEY (`module`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1');
+
 } elseif (isset($INI['sqlite'])  && $INI['sqlite']['active'] == true) {
     $database = new pdo($INI['sqlite']['dsn']);
+    
+//    $database->query('DELETE FROM '.$INI['template.sqlite']['table'].'_rapport WHERE fichier = "'.$fichier.'"');
+    $database->query('CREATE TABLE IF NOT EXISTS '.$INI['template.sqlite']['table'].'_rapport 
+  (id       INTEGER PRIMARY KEY   AUTOINCREMENT  , 
+  `fichier` varchar(500) NOT NULL,
+  `element` varchar(500) NOT NULL,
+  `token_id` int unsigned NOT NULL,
+  `module` varchar(50) NOT NULL
+)');
+        
+//    $database->query('DELETE FROM '.$INI['template.sqlite']['table'].'_rapport_dot WHERE cluster = "'.$fichier.'"');
+    $database->query('CREATE TABLE IF NOT EXISTS '.$INI['template.sqlite']['table'].'_rapport_dot (
+  `a` varchar(255) NOT NULL,
+  `b` varchar(255) NOT NULL,
+  `cluster` varchar(255) NOT NULL DEFAULT \'\',
+  `module` varchar(255) NOT NULL
+)');
+
+    $database->query('CREATE TABLE IF NOT EXISTS '.$INI['template.sqlite']['table'].'_rapport_module (
+  `module` varchar(255) NOT NULL PRIMARY KEY,
+  `fait` datetime NOT NULL,
+  `format` varchar(255) NOT NULL
+)');
 } else {
     print "No database configuration provided (no mysql, no sqlite)\n";
     die();
 }
-// rendu (templates)
+
+// validation done
+
+
+// rendu (templates) @_
 include 'classes/sommaire.php';
 $sommaire = new sommaire();
 
-// abstract classes 
+// @inclusions abstract classes 
 include 'classes/abstract/modules.php';
 include 'classes/abstract/functioncalls.php';
 include 'classes/abstract/typecalls.php';
 include 'classes/abstract/noms.php';
 
-// analyzers doing the real thing
-
-/*
-
-$modules_faits = array();
-$res = $mysql->query('SELECT module FROM '.$prefixe.'_rapport_module');
-$modules_faits = $res->fetchall(PDO::FETCH_COLUMN);
-*/
 $modules_faits = array();
 
-
-// init avec le contenu de la base? 
+// @todo the init could take into account the current content of the database, avoiding reprocess
 
 foreach($modules as $module) {
     print "+ $module\n";
@@ -206,7 +221,7 @@ foreach($modules as $module) {
 
 function analyse_module($module) {
     require_once('classes/'.$module.'.php');
-    global $modules_faits, $database,$sommaire;
+    global $modules_faits, $database,$sommaire, $INI;
     
     if (isset($modules_faits[$module])) {  
         return ;
@@ -220,13 +235,12 @@ function analyse_module($module) {
         if (count($manque) > 0) {
             foreach($manque as $m) {
                 print "  +  $m";
-                if (DO_DEPENDENCES) {
+                if ($INI['dependences']) {
                     analyse_module($m);
                 } else {
                     print " omitted ";
-                    // @todo : check if dependances are there or not. 
-                    // if not, they should be done, of course!
-                    // nothing
+                    // @todo check if dependances are there or not. 
+                    // @todo if not, they should be done, of course!
                 }
                 print "\n";
             }
@@ -250,13 +264,11 @@ Usage : ./auditeur.php
 prefix : tokens (default)
 
     -?    : this help
-    -h    : this help
-    -help : this help
     -a    : comma separated list of analyzers to be used. Defaut to all. 
     -d    : refresh dependent analyzers (default : no)
     -f    : output format : html
     -o    : folder for output : /tmp
-    -p    : prefixe for the tables to be used. Default to 'tokens'
+    -I    : ini config file
 
 TEXT;
     
