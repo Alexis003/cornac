479a42553cfdc6870f101c1e399444e004ee3c53
Seguy Damien <alterway@buildbot32.ingenidev>
2010-09-02 17:17:51 +0200

    fix name

--- tests/auditeur/class._new.test.php
+++ tests/auditeur/class._new.test.php
@@ -1,7 +1,7 @@
 <?php
 include_once('Auditeur_Framework_TestCase.php');
 
-class _New_Test extends Auditeur_Framework_TestCase
+class _new_Test extends Auditeur_Framework_TestCase
 {
     public function testVariables()  { 
         $this->attendus = array('$a','$asp','StdClass', 'classe_sans_parenthese','z');
