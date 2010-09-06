666771910557a5e8e6cefcf28028b0328df989df
Seguy Damien <alterway@AlterWayConsulting.local>
2010-09-06 22:12:01 +0200

    export database connexion

--- auditeur/inventaire.php
+++ auditeur/inventaire.php
@@ -67,14 +67,7 @@ if (empty($INI['reader']['format'])) {
 //$summary = getOption($args, '-s', OPT_NO_VALUE, null);
 
 // @todo put this into a central library 
-if (isset($INI['mysql']) && $INI['mysql']['active'] == true) {
-    $database = new pdo($INI['mysql']['dsn'],$INI['mysql']['username'], $INI['mysql']['password']);
-} elseif (isset($INI['sqlite'])  && $INI['sqlite']['active'] == true) {
-    $database = new pdo($INI['sqlite']['dsn']);
-} else {
-    print "No database configuration provided (no mysql, no sqlite)\n";
-    die();
-}
+include('../libs/database.php');
 
 if (isset($INI['cornac']['prefix'])) {
     $prefix = $INI['cornac']['prefix'];
