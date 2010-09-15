2a97bafa43218d78ee29db5c7be142fd4b5e4588
Seguy Damien <alterway@AlterWayConsulting.local>
2010-09-15 16:47:59 +0200

    fix searching

--- auditeur/classes/multi_def_files.php
+++ auditeur/classes/multi_def_files.php
@@ -12,10 +12,32 @@ class multi_def_files extends modules {
         $this->clean_rapport();
 
 	    $query = <<<SQL
-    CREATE TEMPORARY TABLE multi_def_files
-    SELECT DISTINCT fichier FROM <tokens> 
-        GROUP BY concat(class,'::', scope) 
-        HAVING COUNT(*) > 1
+CREATE TEMPORARY TABLE multi_def_files
+SELECT DISTINCT T1.fichier AS fichier,  if (class= '', scope, class) AS context
+FROM <tokens> T1
+WHERE T1.type NOT IN ('codephp','sequence')
+SQL;
+        $res = $this->exec_query($query);
+
+	    $query = <<<SQL
+INSERT INTO <rapport> 
+    SELECT NULL, T1.fichier, T1.context, 0, '{$this->name}', 0
+    FROM multi_def_files T1
+SQL;
+        $res = $this->exec_query($query);
+
+	    $query = <<<SQL
+DROP TABLE multi_def_files
+SQL;
+        $res = $this->exec_query($query);
+
+/*
+	    $query = <<<SQL
+CREATE TEMPORARY TABLE multi_def_files
+SELECT DISTINCT fichier 
+FROM <tokens> 
+    GROUP BY concat(class,'::', scope) 
+    HAVING COUNT(*) > 1
 SQL;
         $res = $this->exec_query($query);
     
@@ -34,7 +56,7 @@ SQL;
 DROP TABLE multi_def_files
 SQL;
         $res = $this->exec_query($query);
-        
+*/
         return true;
     }
 }
