ead4ffff1d50f2b9ab60969a9f447e16570e5d70
Seguy Damien <alterway@AlterWayConsulting.local>
2010-09-20 10:21:53 +0200

    use unit test

--- web/include/config.php
+++ web/include/config.php
@@ -5,7 +5,7 @@ $ini = array('mysql' => array('active' => 1,
                               'dsn' => 'mysql:dbname=analyseur;host=127.0.0.1', 
                               'username' => 'root',
                               'password' => ''),
-             'cornac' => array('prefix' => 'cornac' ) );
+             'cornac' => array('prefix' => 'tu' ) );
 $DATABASE = new database($ini);
 
 ?>
