06b9b478cd5979e5215c5311519b096c4543ce71
Seguy Damien <alterway@jeanfrancois-ch.siege.sylogis.com>
2010-09-27 16:08:29 +0200

    added disclaimer by default

--- tests/auditeur/skel.php
+++ tests/auditeur/skel.php
@@ -71,7 +71,29 @@ $auditeur = file_get_contents('./alltests.php');
 $auditeur = str_replace("// new tests\n", "'class.$analyzer.test.php',\n// new tests\n", $auditeur);
 file_put_contents('alltests.php', $auditeur);
 
-file_put_contents('scripts/'.$analyzer.'.php', '<?'.'php'."\n\n\n".'?'.'>');
+file_put_contents('scripts/'.$analyzer.'.php', '<?'.'php'.<<<PHP
+
+/*
+   +----------------------------------------------------------------------+
+   | Cornac, PHP code inventory                                           |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2010 Alter Way Solutions (France)                      |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
+   +----------------------------------------------------------------------+
+ */
+
+
+PHP
+."\n\n\n".'?'.'>');
 
 // @synopsis add to git
 shell_exec('git add class.'.$analyzer.'.test.php');
