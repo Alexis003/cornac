6956ea1e41d6f763a63d4f52773783df4ef744b4
Seguy Damien <alterway@jeanfrancois-ch.siege.sylogis.com>
2010-10-01 09:56:03 +0200

    Revert "finalizing delete" should be moved, not deleted.
    This reverts commit 43a002cb173584779a66afeb8a5144c5530b1478.

---
+++ scripts/tags.php
@@ -0,0 +1,423 @@
+#!/usr/bin/env php
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | Cornac, PHP code inventory                                           |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2010 Alter Way Solutions (France)                      |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
+   +----------------------------------------------------------------------+
+ */
+
+// @todo merge comments nexting each other
+
+include('../libs/getopts.php');
+
+$args = $argv;
+if (get_arg($args, '-f')) { define('SHOW_FILES','true'); }
+if (get_arg($args, '-d')) { define('SHOW_DIRS','true'); }
+if (get_arg($args, '-e')) { define('SHOW_DIRS','true'); }
+
+if ($format = get_arg_value($args, '-F', 'print_r')) {
+    if (!in_array($format, array('print_r','csv','xml'))) { $format = 'print_r'; }
+    define('FORMAT', $format);
+}
+if ($dir = get_arg_value($args, '-D', '.')) {
+    $dirs = explode(',', $dir);
+    foreach($dirs as $id => $dir) {
+        if (substr($dir, -1) == '/') {
+            $dirs[$id] = substr($dir, 0, -1);
+        }
+        if (!file_exists($dir)) { 
+            print "'$dir' doesn't exist : ignoring\n"; 
+            unset($dirs[$id]);
+        } else {
+            // @empty_else
+        }
+    }
+    define('DIR', $dir);
+}
+
+$OPTIONS = parse_ini_file("tags.ini");
+if (!isset($OPTIONS['limit']) || $OPTIONS['limit'] == 0) {
+    // @note big value as default
+    $OPTIONS['limit'] = 10000000;
+}
+
+$liste = array();
+foreach($dirs as $dir) {
+    $liste_one_dir = liste_directories_recursive($dir);
+    $liste = array_merge($liste_one_dir, $liste);
+}
+
+$liste = array_slice($liste, 0, $OPTIONS['limit']);
+
+// @question : qoui?
+# @autre : non?
+
+$comments = array();
+foreach($liste as $file) {
+    $php = file_get_contents($file);
+    $tokens = token_get_all($php);
+    $comments_this_file = 0;
+    foreach($tokens as $token) {
+        if (is_array($token) && $token[0] == T_COMMENT) {
+            $comment = remove_delimiter($token[1]);
+
+            // comment used as presentation delimiter (------, ////////, ========)
+            if (preg_match_all('/^\+?(.)\1+$/is', $comment, $r)) {
+                continue;
+            }
+            
+            // Copyright : this is probably a FLOSS disclaimer. Forget it.
+            if (preg_match_all('/Copyright/is', $comment, $r)) {
+                continue;
+            }
+            $comments_this_file++;
+
+            if (preg_match_all('/(@[a-zA-Z0-9_\-]+)/is', $comment, $r)) {
+                $token['tags'] = $r[1];
+                if (in_array('@_', $token['tags'])) {
+                // comments are ignored
+                    continue;
+                }
+            } else {
+                $token['tags'] = array('@no_tag');
+            }
+
+            // @note : identifying code in comments
+            // @todo make this code better : it is not easy to spot PHP code in comments, token_get_all is not sufficent
+            if (strpos($comment, ';')) {
+                $code_php = '<?php '.$comment.' ?>';
+                $t = token_get_all($code_php);
+                if (count($t) != 0) {
+                    $token['tags'][] = '@php_code';
+                }
+            }
+
+            // @note : identifying bad words in comments
+            $regex = join('|', $OPTIONS['smell_words']);
+            if (preg_match('/\b('.$regex.')\b/is', $comment, $r)) {
+               $token['tags'][] = '@smell_words';
+            }
+
+            // @doc clean comment
+            // @note remove all tokens from the comment
+            $token['comment'] = preg_replace('/(@[a-zA-Z0-9_\-]+)/is','', $comment);
+            // @note remove white space and : from comment
+            $token['comment'] = trim($token['comment'], ' :');
+            $token['file'] = $file;
+            unset($token[0]);
+            $token['ligne'] = $token[2];
+            unset($token[2]);
+            $token['raw'] = $comment;
+            unset($token[1]);
+
+            $comments[] = $token;
+        }
+    }
+
+    if ($comments_this_file == 0) {
+        $comments[] = array('file' => $file,
+                            'tags' => array('@no_comment_in_file'),
+                            'comment' => '',);
+    }
+}
+
+chdir(dirname(__FILE__));
+
+export_html($comments);
+export_tags_html($comments);
+export_csv($comments);
+
+die();
+
+function export_csv($comments) {
+    $csv = "";
+
+    $fp = fopen('tags.csv', 'w+');
+
+    foreach($comments as $comment) {
+        $comment['tags'] = join(', ', $comment['tags']);
+        unset($comment['raw']);
+        fputcsv($fp, $comment);
+    }
+    fclose($fp);
+}
+
+function export_html($comments) {
+    setlocale(LC_TIME, "fr_FR");
+    $date = strftime("%A %d %B %Y %T" );
+
+    $css = getCSS();
+    
+    $html = <<<HTML
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="fr">
+    <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+$css
+    </head>
+    <body>
+    <p>Generated on : $date</p>
+HTML;
+
+    $files = $tags = array();
+    foreach($comments as $id => $comment) {
+        $files[$comment['file']][] = $comment;
+        $tags = array_merge($tags, $comment['tags']);
+    }
+    $tags = array_count_values($tags);
+
+    $html .= "    <p>Distinct files : ".count($files)."</p>\n";
+    $html .= "    <p>Distinct comments : ".count($comments)."</p>\n";
+    $html .= "    <p>Distinct tags : ".count($tags)."</p>\n";
+
+
+    $html .= "<table id=\"box-table-a\">\n";
+    foreach($files as $file => $commentaires) {
+        $count = count($commentaires);
+        $html .= "<tr id=\"box-table-a-section\">
+        <td colspan=\"2\"><a name=\"".make_anchor($file)."\"><b>".htmlentities($file)."</b></td>
+        <td>$count</td>
+        </tr>\n";
+        foreach ($commentaires as $id => $commentaire) {
+            $tags = "";
+
+            foreach($commentaire['tags'] as $tag) {
+                $tags .= "<a href=\"tags.html#".make_anchor($tag)."\">".htmlentities($tag)."</a>, ";
+            }
+            $tags = substr($tags, 0, -2);
+            
+            $odd = $id % 2 ? 'odd' : '';
+            $html .= "<tr id=\"$odd\">
+    <td>$id)</td>
+    <td>".wordwrap(htmlspecialchars($commentaire['comment'],ENT_COMPAT, 'UTF-8'), 150, '<br />', TRUE)."</td>
+    <td>$tags</td>
+    </tr>\n";
+        }
+    }
+    $html .= "</table>";
+    $html .= <<<HTML
+    </body>
+</html>
+HTML;
+
+    file_put_contents('files.html', $html);
+    print count($comments)." comments processed\n";
+}
+
+function export_tags_html($comments) {
+    setlocale(LC_TIME, "fr_FR");
+    $date = strftime("%A %d %B %Y %T");
+
+    $css = getCSS();
+    
+    $html = <<<HTML
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="fr">
+    <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+$css
+    </head>
+    <body>
+    <p>Generated on : $date</p>
+HTML;
+
+    $files = $tags = array();
+    foreach($comments as $id => $comment) {
+        $files[] = $comment['file'];
+        foreach($comment['tags'] as $tag) {
+            $tags[$tag][] = $comment;
+        }
+    }
+    
+    $files = array_count_values($files);
+
+    $html .= "    <p>Distinct files : ".count($files)."</p>\n";
+    $html .= "    <p>Distinct comments : ".count($comments)."</p>\n";
+    $html .= "    <p>Distinct tags : ".count($tags)."</p>\n";
+    $html .= "    <p><a name=\"_summary\" />";
+    foreach(array_keys($tags) as $tag) {
+        $html .= "<a href=\"#".make_anchor($tag)."\">$tag</a> - ";
+    } 
+    $html .= "</p>\n";
+    
+
+    $html .= "<table id=\"box-table-a\">\n";
+    foreach($tags as $tag => $commentaires) {
+        $count = count($commentaires);
+        $html .= "<tr id=\"box-table-a-section\">
+            <td colspan=\"2\"><a name=\"".make_anchor($tag)."\"><b>".htmlentities($tag,ENT_COMPAT, 'UTF-8')."</b> [<a href=\"#_summary\">^</a>]</td>
+            <td>$count</td>
+            </tr>\n";
+        foreach ($commentaires as $id => $commentaire) {
+            $odd = $id % 2 ? 'odd' : '';
+            $html .= "<tr id=\"$odd\">
+    <td>$id) </td>
+    <td>".wordwrap(htmlspecialchars($commentaire['comment'],ENT_COMPAT, 'UTF-8'), 150, '<br />', TRUE)."</td>
+    <td><a href=\"files.html#".make_anchor($commentaire['file'])."\">".htmlentities($commentaire['file'],ENT_COMPAT, 'UTF-8')."</a></td>
+    </tr>\n";
+        }
+    }
+    $html .= "</table>";
+    $html .= <<<HTML
+    </body>
+</html>
+HTML;
+
+    print file_put_contents('tags.html', $html)." octets écrits\n";
+}
+
+function remove_delimiter($comment) {
+    $comment = trim($comment);
+
+    if ($comment[0] == '#') {
+        $comment = substr($comment, 1);
+    } elseif (substr($comment, 0, 2) == '//') {
+        $comment = substr($comment, 2);
+    } elseif (substr($comment, 0, 2) == '/*') {
+        $comment = substr($comment, 2, -2);
+    } else {
+        print "Commentaire inconnu : $comment\n";
+    }
+    $comment = trim($comment);
+    return $comment;
+}
+
+$dirs = array_map('dirname', $liste);
+$dirs = array_count_values($dirs);
+if (SHOW_DIRS) { display($dirs); }
+
+$files = array_map('basename', $liste);
+$files = array_count_values($files);
+if (SHOW_FILES) { display($files); }
+
+$exts = array_map('cb_exts', $liste);
+$exts = array_count_values($exts);
+if (SHOW_EXTS) { display($exts); }
+
+//print count($liste)." files distincts\n";
+
+function liste_directories_recursive( $path = '.', $level = 0 ){
+    global $OPTIONS;
+    $ignore_dirs = array_merge(array( 'cgi-bin', '.', '..' ), $OPTIONS['ignore_dirs']);
+
+    $dh = opendir( $path );
+    if (!is_resource($dh)) { return array(); }
+    $retour = array();
+    while( false !== ( $file = readdir( $dh ) ) ) {
+        if( $file[0] == '.'                ){ continue; }
+
+        if( is_dir( "$path/$file" ) ){
+            if( in_array( $file, $ignore_dirs )){ continue; }
+            $r = Liste_directories_recursive( "$path/$file", ($level+1) );
+            $retour = array_merge($retour, $r);
+        } else {
+            $details = pathinfo($file);
+            if (!isset($details['extension'])){
+                $details['extension'] = '';
+            }
+            if (in_array($details['extension'], $OPTIONS['ignore_ext'])) { continue; }
+
+            $retour[] = "$path/$file";
+        }
+        if ($OPTIONS['limit'] > 0 && count($retour) >= $OPTIONS['limit']) {
+            return $retour;
+        }
+    }
+
+    closedir( $dh );
+    return $retour;
+}
+
+function make_anchor($name) {
+    $name = preg_replace('/[^a-zA-Z]/', '_', $name);
+    $name = preg_replace('/_+/', '_', $name);
+    $name = trim($name, '_');
+    return $name;
+}
+
+function cb_exts($filename) {
+    $filename = basename($filename);
+    $pos = strrpos($filename, '.');
+    return substr($filename, $pos);
+}
+
+function display($list) {
+    if (FORMAT == 'print_r') {
+        print_r($list);
+    } elseif (FORMAT == 'xml') {
+        print "<list>\n    <file>".join("</file>\n    <file>", $list)."</file>\n</list>\n";
+    } elseif (FORMAT == 'csv') {
+        print '"'.join("\"\n\"", $list).'"';
+    }
+
+    return true;
+}
+
+function getCSS() {
+    return <<<CSS
+<style type="text/css">
+#box-table-a
+{
+	font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+	font-size: 12px;
+	margin: 45px;
+	width: 880px;
+	text-align: left;
+	border-collapse: collapse;
+}
+#box-table-a th
+{
+	font-size: 13px;
+	font-weight: normal;
+	padding: 8px;
+	background: #b9c9fe;
+	border-top: 4px solid #aabcfe;
+	border-bottom: 1px solid #fff;
+	color: #039;
+}
+#box-table-a td
+{
+	padding: 8px;
+	background: #e8edff; 
+	border-bottom: 1px solid #fff;
+	color: #000000;
+	border-top: 1px solid transparent;
+	
+}
+
+#odd td
+{
+	background: #f9feff; 
+#	background: #e8edff; 
+	
+}
+
+#box-table-a-section td
+{
+	background: #cccccc; 
+	
+}
+
+#box-table-a tr:hover td
+{
+	background: #d0dafd;
+	color: #339;
+}
+</style>
+CSS;
+}
+?>
