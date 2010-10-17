fc7df2173eba239ab0c982e20071518724e695a6
Seguy Damien <alterway@AlterWayConsulting.local>
2010-10-17 09:02:01 +0200

    added new test

--- tests/tokenizeur/class.sign.test.php
+++ tests/tokenizeur/class.sign.test.php
@@ -1,26 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */
 include_once('Analyseur_Framework_TestCase.php');
 
 class Sign_Test extends Analyseur_Framework_TestCase
 {
-    /* 21 methodes */
+    /* 22 methodes */
     public function testSign1()  { $this->generic_test('sign.1'); }
     public function testSign2()  { $this->generic_test('sign.2'); }
     public function testSign3()  { $this->generic_test('sign.3'); }
@@ -42,6 +25,7 @@ class Sign_Test extends Analyseur_Framework_TestCase
     public function testSign19()  { $this->generic_test('sign.19'); }
     public function testSign20()  { $this->generic_test('sign.20'); }
     public function testSign21()  { $this->generic_test('sign.21'); }
+    public function testSign22()  { $this->generic_test('sign.22'); }
 
 }
 
