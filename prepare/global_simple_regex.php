4376bcd55fa5eee9a3c92f3e57086b4ca1c4205e
Seguy Damien <alterway@AlterWayConsulting.local>
2010-10-24 09:18:45 +0200

    reworked code for better reading

--- prepare/global_simple_regex.php
+++ prepare/global_simple_regex.php
@@ -27,47 +27,39 @@ class global_simple_regex extends analyseur_regex {
     }
     
     function check($t) {
-        if (!$t->hasNext(2)) { return false; }
+        if (!$t->hasNext(1)) { return false; }
 
-        if ($t->checkToken(array(T_GLOBAL)) &&
-            $t->getNext()->checkClass('variable')  
-            ) {
+        if ($t->getNext()->checkNotClass('variable')) { return false; }
 
-                $var = $t->getNext(1);
+        $var = $t->getNext(1);
 
-                while($var->checkCode(',')) {
-                    if ($var->getNext()->checkClass('variable')) {
-                        
-                            $args = array(0);
-                            $remove = array(1);
-                            
-                            $repl = $var->getNext();
-                            $var = $var->getNext(1);
-                            
-                            $regex = new modele_regex('_global',$args, $remove);
-                            Token::applyRegex($repl, '_global', $regex);
-                            continue;
-                        }
-                    // @note if we reach here, then something is wrong
-                    return false;
+        while($var->checkCode(',')) {
+            if ($var->getNext()->checkClass('variable')) {
+                // @note registering a new global each comma
+                    $args = array(0);
+                    $remove = array(1);
+                    
+                    $repl = $var->getNext();
+                    $var = $var->getNext(1);
+                    
+                    $regex = new modele_regex('_global',$args, $remove);
+                    Token::applyRegex($repl, '_global', $regex);
+                    continue;
                 }
-                
-                if ( $var->checkCode(';')) {
-                    $this->args   = array(1);
-                    $this->remove = array(1, 2);
-
-                    mon_log(get_class($t)." => ".__CLASS__);
-                    return true; 
-                }
-                
-                $this->args = array(1);
-                $this->remove = array(1);
-
-                mon_log(get_class($t)." => ".__CLASS__);
-                return true;
-        } else {
+            // @note if we reach here, then something is wrong
             return false;
         }
+        
+        if ( $var->checkCode(';')) {
+            $this->args   = array(1);
+            $this->remove = array(1, 2);
+
+            mon_log(get_class($t)." => ".__CLASS__);
+            return true; 
+        }
+        
+        // @note otherwise, fail
+        return false;
     }
 }
 
