4780191435ed880ac791d2cdca82000c09ec2bf7
Seguy Damien <alterway@alterwayconsulting.livebox.home>
2010-10-27 22:58:02 +0200

    new tests, from OS tests

---
+++ tests/tokenizeur/scripts/cdtternaire.21.test.php
@@ -0,0 +1,13 @@
+<?php
+
+	if (!empty($a) && $b->c(d))
+	{
+		if ($e->f(g))
+		{
+			return 1;
+		}
+	}
+
+	(empty($x['t'])) ? @flush() : @ob_flush();
+
+?>
---
+++ tests/tokenizeur/scripts/sign.23.test.php
@@ -0,0 +1,3 @@
+<?php
+	$list = - Stdclass::methode("arg");
+?>
