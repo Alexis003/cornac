fc49ad20ae8064f7bf11bdcbd022b9aa042c80ac
Seguy Damien <alterway@AlterWayConsulting.local>
2010-10-29 17:32:39 +0200

    support casting return value of variable named function

--- prepare/cast_normal_regex.php
+++ prepare/cast_normal_regex.php
@@ -24,35 +24,26 @@ class cast_normal_regex extends analyseur_regex {
 
     function getTokens() {
         return array(T_INT_CAST, 
-                                 T_DOUBLE_CAST, 
-                                 T_STRING_CAST,
-                                 T_ARRAY_CAST,
-                                 T_BOOL_CAST,
-                                 T_OBJECT_CAST,
-                                 T_UNSET_CAST);
+                     T_DOUBLE_CAST, 
+                     T_STRING_CAST,
+                     T_ARRAY_CAST,
+                     T_BOOL_CAST,
+                     T_OBJECT_CAST,
+                     T_UNSET_CAST);
     }
     
     function check($t) {
         if (!$t->hasNext()) { return false; }
 
-        if ($t->checkToken(array(T_INT_CAST, 
-                                 T_DOUBLE_CAST, 
-                                 T_STRING_CAST,
-                                 T_ARRAY_CAST,
-                                 T_BOOL_CAST,
-                                 T_OBJECT_CAST,
-                                 T_UNSET_CAST)) &&
-            $t->getNext()->checkNotClass('Token') && 
-            ($t->getNext(1)->checkNotCode(array('[','->','{','::')))
-            ) {
+        if ($t->getNext()->checkClass('Token')) { return false; }
+        if ($t->getNext(1)->checkCode(array('[','->','{','::','('))) { return false; }
+        if ($t->getNext(1)->checkClass('arglist')) { return false; }
 
-            $this->args = array(0, 1);
-            $this->remove = array(1);
+        $this->args = array(0, 1);
+        $this->remove = array(1);
 
-            mon_log(get_class($t)." => ".__CLASS__);
-            return true; 
-        } 
-        return false;
+        mon_log(get_class($t)." => ".__CLASS__);
+        return true; 
     }
 }
 ?>
