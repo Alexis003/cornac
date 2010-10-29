fbcba7472a1b226e08e2c0b1054c0a58ddc63c23
Seguy Damien <alterway@AlterWayConsulting.local>
2010-10-29 17:31:47 +0200

    new test, when mixing variable function name and cast

--- tests/tokenizeur/class.cast.test.php
+++ tests/tokenizeur/class.cast.test.php
@@ -1,26 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */
 include_once('Analyseur_Framework_TestCase.php');
 
 class Cast_Test extends Analyseur_Framework_TestCase
 {
-    /* 13 methodes */
+    /* 15 methodes */
     public function testCast1()  { $this->generic_test('cast.1'); }
     public function testCast2()  { $this->generic_test('cast.2'); }
     public function testCast3()  { $this->generic_test('cast.3'); }
@@ -34,6 +17,8 @@ class Cast_Test extends Analyseur_Framework_TestCase
     public function testCast11()  { $this->generic_test('cast.11'); }
     public function testCast12()  { $this->generic_test('cast.12'); }
     public function testCast13()  { $this->generic_test('cast.13'); }
+    public function testCast14()  { $this->generic_test('cast.14'); }
+    public function testCast15()  { $this->generic_test('cast.15'); }
 
 }
 
