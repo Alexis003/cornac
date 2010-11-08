799f735dc407577116e29ab8e1d72830850d1e9d
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2010-11-08 15:49:35 +0100

    include while and dowhile in iffectactions

--- auditeur/classes/iffectations.php
+++ auditeur/classes/iffectations.php
@@ -18,8 +18,8 @@
  */
 
 class iffectations extends modules {
-	protected	$title = 'Affectations dans un if';
-	protected	$description = 'Affectation dans un if';
+	protected	$title = 'Affectations in a if';
+	protected	$description = 'Affectation in a if, or a while';
 
 	function __construct($mid) {
         parent::__construct($mid);
@@ -28,22 +28,57 @@ class iffectations extends modules {
 	public function analyse() {
         $this->clean_rapport();
 
+// @note in a if
 	    $query = <<<SQL
-
 SELECT NULL, T1.fichier, TC.code, T1.id, '{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens_tags> TT
-  ON T1.id = TT.token_id 
+    ON T1.id = TT.token_id 
 JOIN <tokens> T2
-  ON T1.fichier = T2.fichier AND TT.token_sub_id = T2.id
+    ON T1.fichier = T2.fichier AND TT.token_sub_id = T2.id
 JOIN <tokens> T3
-  ON T1.fichier = T3.fichier AND T3.droite BETWEEN T2.droite AND T2.gauche
+    ON T1.fichier = T3.fichier AND T3.droite BETWEEN T2.droite AND T2.gauche
 JOIN <tokens>_cache TC
-  ON TC.id = T3.id
-  WHERE T1.type='ifthen' AND
-        TT.type = 'condition' AND
-        T3.type = 'affectation' AND
-        T1.fichier = './tests/auditeur/scripts/iffectations.php';
+    ON TC.id = T3.id
+WHERE T1.type='ifthen' AND
+      TT.type = 'condition' AND
+      T3.type = 'affectation'
+SQL;
+        $this->exec_query_insert('rapport', $query);
+
+// @note in a while
+	    $query = <<<SQL
+SELECT NULL, T1.fichier, TC.code, T1.id, '{$this->name}', 0
+FROM <tokens> T1
+JOIN <tokens_tags> TT
+    ON T1.id = TT.token_id 
+JOIN <tokens> T2
+    ON T1.fichier = T2.fichier AND TT.token_sub_id = T2.id
+JOIN <tokens> T3
+    ON T1.fichier = T3.fichier AND T3.droite BETWEEN T2.droite AND T2.gauche
+JOIN <tokens>_cache TC
+    ON TC.id = T3.id
+WHERE T1.type='_while' AND
+      TT.type = 'condition' AND
+      T3.type = 'affectation'
+SQL;
+        $this->exec_query_insert('rapport', $query);
+
+// @note in a do while
+	    $query = <<<SQL
+SELECT NULL, T1.fichier, TC.code, T1.id, '{$this->name}', 0
+FROM <tokens> T1
+JOIN <tokens_tags> TT
+    ON T1.id = TT.token_id 
+JOIN <tokens> T2
+    ON T1.fichier = T2.fichier AND TT.token_sub_id = T2.id
+JOIN <tokens> T3
+    ON T1.fichier = T3.fichier AND T3.droite BETWEEN T2.droite AND T2.gauche
+JOIN <tokens>_cache TC
+    ON TC.id = T3.id
+WHERE T1.type='_dowhile' AND
+      TT.type = 'condition' AND
+      T3.type = 'affectation'
 SQL;
         $this->exec_query_insert('rapport', $query);
         
