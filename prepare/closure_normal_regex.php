917a631e62f64754a483b2af3a3a8d07ce673e57
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-10 06:56:39 +0100

    suppport for closures

---
+++ prepare/closure_normal_regex.php
@@ -0,0 +1,56 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | Cornac, PHP code inventory                                           |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2010 Alter Way Solutions (France)                      |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
+   +----------------------------------------------------------------------+
+ */
+
+class closure_normal_regex extends analyseur_regex {
+    function __construct() {
+        parent::__construct(array());
+    }
+
+    function getTokens() {
+        return array(T_FUNCTION);
+    }
+    
+    function check($t) {
+        if (!$t->hasNext(2)) { return false; }
+
+        $this->args = array();
+        $this->remove = array(1,2);
+
+        if ($t->getNext()->checkNotOperator(array('('))) { return false; }
+        if ($t->getNext(1)->checkNotOperator(array(')'))) { return false; }
+        $pos = 2;
+        if ($t->getNext($pos)->checkToken(T_USE) &&
+            $t->getNext($pos + 1)->checkClass('arglist')) {
+                
+            $this->args[] = $pos + 2;
+            $this->remove[] = $pos + 1;
+            $this->remove[] = $pos + 2;
+            
+            $pos += 2;
+        }
+
+        if ($t->getNext($pos)->checkNotClass(array('block'))) { return false; }
+        $this->args[] = $pos + 1;
+        $this->remove[] = $pos + 1;
+
+        mon_log(get_class($t)." => ".__CLASS__);
+        return true; 
+    }
+}
+?>
