fce33f3bb5aa947156b45315fc7cf464eb5672f9
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-11 12:48:59 +0100

    new tests against NSspace in arrays

--- tests/tokenizeur/class.arrayfct.test.php
+++ tests/tokenizeur/class.arrayfct.test.php
@@ -1,38 +1,22 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */
 include_once('Analyseur_Framework_TestCase.php');
 
-class arrayfct_Test extends Analyseur_Framework_TestCase
+class Arrayfct_Test extends Analyseur_Framework_TestCase
 {
-    /* 12 methodes */
-    public function testarrayfct1()  { $this->generic_test('arrayfct.1'); }
-    public function testarrayfct2()  { $this->generic_test('arrayfct.2'); }
-    public function testarrayfct3()  { $this->generic_test('arrayfct.3'); }
-    public function testarrayfct4()  { $this->generic_test('arrayfct.4'); }
-    public function testarrayfct5()  { $this->generic_test('arrayfct.5'); }
-    public function testarrayfct6()  { $this->generic_test('arrayfct.6'); }
-    public function testarrayfct7()  { $this->generic_test('arrayfct.7'); }
-    public function testarrayfct8()  { $this->generic_test('arrayfct.8'); }
-    public function testarrayfct9()  { $this->generic_test('arrayfct.9'); }
-    public function testarrayfct10()  { $this->generic_test('arrayfct.10'); }
-    public function testarrayfct11()  { $this->generic_test('arrayfct.11'); }
-    public function testarrayfct12()  { $this->generic_test('arrayfct.12'); }
+    /* 13 methodes */
+    public function testArrayfct1()  { $this->generic_test('arrayfct.1'); }
+    public function testArrayfct2()  { $this->generic_test('arrayfct.2'); }
+    public function testArrayfct3()  { $this->generic_test('arrayfct.3'); }
+    public function testArrayfct4()  { $this->generic_test('arrayfct.4'); }
+    public function testArrayfct5()  { $this->generic_test('arrayfct.5'); }
+    public function testArrayfct6()  { $this->generic_test('arrayfct.6'); }
+    public function testArrayfct7()  { $this->generic_test('arrayfct.7'); }
+    public function testArrayfct8()  { $this->generic_test('arrayfct.8'); }
+    public function testArrayfct9()  { $this->generic_test('arrayfct.9'); }
+    public function testArrayfct10()  { $this->generic_test('arrayfct.10'); }
+    public function testArrayfct11()  { $this->generic_test('arrayfct.11'); }
+    public function testArrayfct12()  { $this->generic_test('arrayfct.12'); }
+    public function testArrayfct13()  { $this->generic_test('arrayfct.13'); }
 
 }
 
