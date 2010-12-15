3f6620c8df5513c4dea6c9eb3ad5999e26fac892
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-15 21:16:35 +0100

    one can use sign on a not

--- prepare/sign_regex.php
+++ prepare/sign_regex.php
@@ -33,7 +33,7 @@ class sign_regex extends analyseur_regex {
         if ( $t->getNext()->checkNotClass(array('variable','property','_array', 'property_static',
                                                 'method','method_static','functioncall',
                                                 'constante','constante_static','literals',
-                                                'parentheses', 'operation', 'cast'))) { return false; }
+                                                'parentheses', 'operation', 'cast','not'))) { return false; }
         if ( $t->getPrev()->checkClass(array('literals','variable','_array',
                                              'property','operation','sign',
                                              'functioncall','parentheses','arglist',
