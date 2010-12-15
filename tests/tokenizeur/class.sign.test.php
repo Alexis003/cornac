ae837e73256fb973ff0f4036352cab02db65ddfd
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-15 21:16:16 +0100

    one can use sign on a not

--- tests/tokenizeur/class.sign.test.php
+++ tests/tokenizeur/class.sign.test.php
@@ -1,25 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */include_once('Analyseur_Framework_TestCase.php');
+include_once('Analyseur_Framework_TestCase.php');
 
 class Sign_Test extends Analyseur_Framework_TestCase
 {
-    /* 28 methodes */
+    /* 29 methodes */
     public function testSign1()  { $this->generic_test('sign.1'); }
     public function testSign2()  { $this->generic_test('sign.2'); }
     public function testSign3()  { $this->generic_test('sign.3'); }
@@ -48,6 +32,7 @@ class Sign_Test extends Analyseur_Framework_TestCase
     public function testSign26()  { $this->generic_test('sign.26'); }
     public function testSign27()  { $this->generic_test('sign.27'); }
     public function testSign28()  { $this->generic_test('sign.28'); }
+    public function testSign29()  { $this->generic_test('sign.29'); }
 
 }
 
