70f91153edd9ad021f24f2e472af8ec99c41d299
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-15 07:14:16 +0100

    translations to English

--- tests/tokenizeur/class.array.test.php
+++ tests/tokenizeur/class.array.test.php
@@ -1,25 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */include_once('Analyseur_Framework_TestCase.php');
+include_once('Analyseur_Framework_TestCase.php');
 
 class Array_Test extends Analyseur_Framework_TestCase
 {
-    /* 13 methodes */
+    /* 14 methodes */
     public function testArray1()  { $this->generic_test('array.1'); }
     public function testArray2()  { $this->generic_test('array.2'); }
     public function testArray3()  { $this->generic_test('array.3'); }
@@ -33,6 +17,7 @@ class Array_Test extends Analyseur_Framework_TestCase
     public function testArray11()  { $this->generic_test('array.11'); }
     public function testArray12()  { $this->generic_test('array.12'); }
     public function testArray13()  { $this->generic_test('array.13'); }
+    public function testArray14()  { $this->generic_test('array.14'); }
 
 }
 
