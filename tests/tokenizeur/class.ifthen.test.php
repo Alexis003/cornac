b561670a885477466486437ccb18aa6c9e2d887d
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-15 17:08:07 +0100

    ifthen may accept parenthesis as blocks!

--- tests/tokenizeur/class.ifthen.test.php
+++ tests/tokenizeur/class.ifthen.test.php
@@ -1,25 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */include_once('Analyseur_Framework_TestCase.php');
+include_once('Analyseur_Framework_TestCase.php');
 
 class Ifthen_Test extends Analyseur_Framework_TestCase
 {
-    /* 60 methodes */
+    /* 62 methodes */
     public function testIfthen1()  { $this->generic_test('ifthen.1'); }
     public function testIfthen2()  { $this->generic_test('ifthen.2'); }
     public function testIfthen3()  { $this->generic_test('ifthen.3'); }
@@ -80,6 +64,8 @@ class Ifthen_Test extends Analyseur_Framework_TestCase
     public function testIfthen58()  { $this->generic_test('ifthen.58'); }
     public function testIfthen59()  { $this->generic_test('ifthen.59'); }
     public function testIfthen60()  { $this->generic_test('ifthen.60'); }
+    public function testIfthen61()  { $this->generic_test('ifthen.61'); }
+    public function testIfthen62()  { $this->generic_test('ifthen.62'); }
 
 }
 
