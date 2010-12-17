4c51577fa0c0e125a3feee0c17120e1f7e46117e
Seguy Damien <alterway@mdnnb-037.siege.sylogis.com>
2010-12-17 10:18:02 +0100

    added test for logical + sign

--- tests/tokenizeur/class.sign.test.php
+++ tests/tokenizeur/class.sign.test.php
@@ -3,7 +3,7 @@ include_once('Analyseur_Framework_TestCase.php');
 
 class Sign_Test extends Analyseur_Framework_TestCase
 {
-    /* 30 methodes */
+    /* 31 methodes */
     public function testSign1()  { $this->generic_test('sign.1'); }
     public function testSign2()  { $this->generic_test('sign.2'); }
     public function testSign3()  { $this->generic_test('sign.3'); }
@@ -34,6 +34,7 @@ class Sign_Test extends Analyseur_Framework_TestCase
     public function testSign28()  { $this->generic_test('sign.28'); }
     public function testSign29()  { $this->generic_test('sign.29'); }
     public function testSign30()  { $this->generic_test('sign.30'); }
+    public function testSign31()  { $this->generic_test('sign.31'); }
 
 }
 
