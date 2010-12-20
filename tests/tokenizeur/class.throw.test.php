2fc3ca584ea182777508bcf0004e26eb93bdd355
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-20 07:04:42 +0100

    2 new situations for throw

--- tests/tokenizeur/class.throw.test.php
+++ tests/tokenizeur/class.throw.test.php
@@ -1,26 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */
 include_once('Analyseur_Framework_TestCase.php');
 
 class Throw_Test extends Analyseur_Framework_TestCase
 {
-    /* 8 methodes */
+    /* 10 methodes */
     public function testThrow1()  { $this->generic_test('throw.1'); }
     public function testThrow2()  { $this->generic_test('throw.2'); }
     public function testThrow3()  { $this->generic_test('throw.3'); }
@@ -29,6 +12,8 @@ class Throw_Test extends Analyseur_Framework_TestCase
     public function testThrow6()  { $this->generic_test('throw.6'); }
     public function testThrow7()  { $this->generic_test('throw.7'); }
     public function testThrow8()  { $this->generic_test('throw.8'); }
+    public function testThrow9()  { $this->generic_test('throw.9'); }
+    public function testThrow10()  { $this->generic_test('throw.10'); }
 
 }
 
---
+++ tests/tokenizeur/scripts/throw.9.test.php
@@ -0,0 +1,3 @@
+<?php 
+  throw ($e ? $e : new a('b'));
+?>
