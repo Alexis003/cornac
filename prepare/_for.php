cefa345e4e4c9558180388de56cd43f0c0cbb725
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2010-12-21 15:09:58 +0100

    removed useless code

--- prepare/_for.php
+++ prepare/_for.php
@@ -19,7 +19,7 @@
 
 class _for extends instruction {
     protected $init = null;
-    protected $fin = null;
+    protected $end = null;
     protected $increment = null;
     protected $block = null;
 
@@ -35,23 +35,14 @@ class _for extends instruction {
         // @note immediate processing of block
         $this->block = array_pop($expression);
 
-        if ($expression[1]->checkClass('sequence') && $expression[2]->checkCode(')')) {
+        if ($expression[1]->getCode() == ';') {
+            $this->end = null;
+        } elseif ($expression[1]->checkClass('sequence') == 'sequence') {
             $elements = $expression[1]->getElements();
-            if (count($elements) == 2) { 
-                $this->fin = $x[0];
-                $this->increment = $x[1];
-                
-                return;
-            } elseif (count($x) == 1) {
-                $this->fin = $x[0];
-                // @for_translation puis on continue comme d'hab, increment est dans expression[2];
-            } else {
-                $this->stopOnError("Wrong number of elements  : '".count($x)."' in ".__METHOD__);
-            }
-        } elseif ($expression[1]->getCode() == ';') {
-            $this->fin = null;
+            $this->end = $elements[0];
+            // @note what if we get several elements?
         } else {
-            $this->fin = $expression[1];
+            $this->end = $expression[1];
         }
         
         if ($expression[2]->getCode() == ')') {
@@ -63,15 +54,15 @@ class _for extends instruction {
     }
     
     function __toString() {
-        return __CLASS__." for (".$this->init."; ".$this->fin."; ".$this->increment." ) {".$this->block."} ";
+        return __CLASS__." for (".$this->init."; ".$this->end."; ".$this->increment." ) {".$this->block."} ";
     }
 
     function getInit() {
         return $this->init;
     }
 
-    function getFin() {
-        return $this->fin;
+    function getEnd() {
+        return $this->end;
     }
 
     function getIncrement() {
@@ -86,8 +77,8 @@ class _for extends instruction {
         if (!is_null($this->init)) {
             $this->init->detach();
         }
-        if (!is_null($this->fin)) {
-            $this->fin->detach();
+        if (!is_null($this->end)) {
+            $this->end->detach();
         }
         if (!is_null($this->increment)) {
             $this->increment->detach();
