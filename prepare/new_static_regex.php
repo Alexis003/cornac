ff16bab56949593cd3dd362590ecb2a96cf87f3b
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-28 17:52:51 +0100

    support for the lsb call static

---
+++ prepare/new_static_regex.php
@@ -0,0 +1,60 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | Cornac, PHP code inventory                                           |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2010 Alter Way Solutions (France)                      |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
+   +----------------------------------------------------------------------+
+ */
+
+class new_static_regex extends analyseur_regex {
+    function __construct() {
+        parent::__construct(array());
+    }
+
+    function getTokens() {
+        return array(T_NEW);
+    }
+    
+    function check($t) {
+        if (!$t->hasNext(1)) { return false; }
+
+        if ($t->getNext()->checkNotToken(array(T_STATIC))) { return false; }
+
+        if (!$t->getNext(1)->checkEndInstruction() &&
+            !$t->getNext(1)->checkForLogical()) { return false; }
+
+        $this->args = array(1);
+        $this->remove = array(1);
+        
+        if ( $t->hasNext(3) &&
+             $t->getNext(1)->checkCode('(') &&
+             $t->getNext(2)->checkCode(')')
+             ) {
+
+            $this->args[]   = 2;
+            $this->args[]   = 3;
+            $this->remove[] = 2;
+            $this->remove[] = 3;
+       } 
+
+        if ( $t->getNext(1)->checkClass('parentheses')) {
+            $this->args[]   = 2;
+            $this->remove[] = 2;
+        } 
+
+        mon_log(get_class($t)." => ".__CLASS__);
+        return true; 
+    }
+}
+?>
