3671ee81a7597264957f3b9cb3a41b73641e098a
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-31 13:40:33 +0100

    border case for constants

--- tests/tokenizeur/class.constante.test.php
+++ tests/tokenizeur/class.constante.test.php
@@ -3,7 +3,7 @@ include_once('Analyseur_Framework_TestCase.php');
 
 class Constante_Test extends Analyseur_Framework_TestCase
 {
-    /* 7 methodes */
+    /* 8 methodes */
     public function testConstante1()  { $this->generic_test('constante.1'); }
     public function testConstante2()  { $this->generic_test('constante.2'); }
     public function testConstante3()  { $this->generic_test('constante.3'); }
@@ -11,6 +11,7 @@ class Constante_Test extends Analyseur_Framework_TestCase
     public function testConstante5()  { $this->generic_test('constante.5'); }
     public function testConstante6()  { $this->generic_test('constante.6'); }
     public function testConstante7()  { $this->generic_test('constante.7'); }
+    public function testConstante8()  { $this->generic_test('constante.8'); }
 
 }
 
