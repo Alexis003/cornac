661481b2da9ac170d5750763970e8508b773a8b4
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-31 15:28:22 +0100

    refactored code

--- prepare/case_between_regex.php
+++ prepare/case_between_regex.php
@@ -29,66 +29,66 @@ class case_between_regex extends analyseur_regex {
     function check($t) {
         if (!$t->hasNext()) { return false; }
 
-        if ($t->checkGenericCase()) {
-            if ($t->checkToken(T_CASE)) {
-                if ($t->getNext()->checkClass(array('Token'))) { return false; }
-                $var = $t->getNext(2);
-                $init = $t->getNext(2);
+        if (!$t->checkGenericCase()) { return false; }
+        
+        if ($t->checkToken(T_CASE)) {
+            if ($t->getNext()->checkClass(array('Token'))) { return false; }
+            $var = $t->getNext(2);
+            $init = $t->getNext(2);
 
-                $this->args = array(0, 1 );
-                $this->remove = array(1,2);
-            } elseif ($t->checkToken(T_DEFAULT) || $t->checkClass('_default')) {
-                $var = $t->getNext(1);
-                $init = $t->getNext(1);
+            $this->args = array(0, 1 );
+            $this->remove = array(1,2);
+        } elseif ($t->checkToken(T_DEFAULT) || $t->checkClass('_default')) {
+            $var = $t->getNext(1);
+            $init = $t->getNext(1);
 
-                $this->args = array(0 );
-                $this->remove = array(1);
-            } elseif ($t->checkClass('_case')) {
-                return false; 
-            } else {
-                mon_log("Trying to spot ".__CLASS__." => block but '".$t."' is not T_CASE, nor T_DEFAULT");
-                return false;
-            }
-            $args = array();
-            $remove = array();
-            $pos = 0;
-            
-            while(!$var->checkGenericCase() && 
-                   $var->checkNotCode('}')  && 
-                   $var->checkNotToken(T_ENDSWITCH)) {
-                if ($var->checkCode(';')) { 
-                    $remove[] = $pos;
-                    $pos++;
-                    $var = $var->getNext();
-                    continue;
-                }    
-                // @note waiting for all structures to be processed
-                if ($var->checkClass(array('Token')))                      { return false; }
-                if ($var->checkCode('{') && $var->checkNotClass('block'))  { return false; } 
+            $this->args = array(0 );
+            $this->remove = array(1);
+        } elseif ($t->checkClass(array('_case','_default'))) {
+            return false; 
+        } else {
+            mon_log("Trying to spot ".__CLASS__." => block but '".$t."' is not T_CASE, nor T_DEFAULT");
+            return false;
+        }
+        $args = array();
+        $remove = array();
+        $pos = 0;
+        
+        while(!$var->checkGenericCase() && 
+               $var->checkNotCode('}')  && 
+               $var->checkNotToken(T_ENDSWITCH)) {
 
-                $args[] = $pos;
+            if ($var->checkCode(';')) { 
                 $remove[] = $pos;
                 $pos++;
                 $var = $var->getNext();
-            }
-            
-            if (empty($args) && empty($remove)) { 
-                // @note empty cas, but case nonetheless. 
-                mon_log(get_class($t)." => ".__CLASS__);
-                return true; 
-            } 
+                continue;
+            }    
+            // @note waiting for all structures to be processed
+            if ($var->checkClass(array('Token')))                      { return false; }
+            if ($var->checkCode('{') && $var->checkNotClass('block'))  { return false; } 
 
-            $regex = new modele_regex('block',$args, $remove);
-            Token::applyRegex($init, 'block', $regex);
+            $args[] = $pos;
+            $remove[] = $pos;
+            $pos++;
+            $var = $var->getNext();
+        }
 
-            mon_log(get_class($t)." => block (".__CLASS__.")");
+        if (empty($args) && empty($remove)) { 
+            // @note empty case, but case nonetheless. 
+            mon_log(get_class($t)." => ".__CLASS__);
+            return true; 
+        }
 
-            $this->args = $args;
-            $this->remove = $remove;
+        $regex = new modele_regex('block',$args, $remove);
+        Token::applyRegex($init, 'block', $regex);
 
-            return false; 
-        } 
-        return false;
+        mon_log(get_class($t)." => block (".__CLASS__.")");
+
+        $this->args = $args;
+        $this->remove = $remove;
+
+        return false; 
     }
 }
 ?>
