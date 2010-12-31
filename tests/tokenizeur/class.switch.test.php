a41ca740903d1ff942dbd816d4835d604a004872
Seguy Damien <alterway@AlterWayConsulting.local>
2010-12-31 15:27:46 +0100

    new tests for swithc

--- tests/tokenizeur/class.switch.test.php
+++ tests/tokenizeur/class.switch.test.php
@@ -1,25 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 Alter Way Solutions (France)                      |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */include_once('Analyseur_Framework_TestCase.php');
+include_once('Analyseur_Framework_TestCase.php');
 
 class Switch_Test extends Analyseur_Framework_TestCase
 {
-    /* 27 methodes */
+    /* 28 methodes */
     public function testSwitch1()  { $this->generic_test('switch.1'); }
     public function testSwitch2()  { $this->generic_test('switch.2'); }
     public function testSwitch3()  { $this->generic_test('switch.3'); }
@@ -47,6 +31,7 @@ class Switch_Test extends Analyseur_Framework_TestCase
     public function testSwitch25()  { $this->generic_test('switch.25'); }
     public function testSwitch26()  { $this->generic_test('switch.26'); }
     public function testSwitch27()  { $this->generic_test('switch.27'); }
+    public function testSwitch28()  { $this->generic_test('switch.28'); }
 
 }
 
---
+++ tests/tokenizeur/scripts/switch.29.test.php
@@ -0,0 +1,10 @@
+<?php
+switch ( true )
+{
+    case $a->b === null:
+        return new c( $d );
+
+    default:
+        return new e( $f );
+}
+?>
