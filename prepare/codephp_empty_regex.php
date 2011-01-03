53be987a7419150096324be7f76088e0789209a0
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2011-01-03 13:25:12 +0100

    refactored code

--- prepare/codephp_empty_regex.php
+++ prepare/codephp_empty_regex.php
@@ -29,21 +29,18 @@ class codephp_empty_regex extends analyseur_regex {
     function check($t) {
         if (!$t->hasNext()) { return false; }
 
-        if ($t->checkToken(T_OPEN_TAG) &&
-            $t->getNext()->checkToken(T_CLOSE_TAG)) {
-            
-            if ($t->hasNext(1) && 
-                $t->getNext(1)->checkToken(T_INLINE_HTML)) {
-                    // @note avoid polluting rawtext_regex 
-                return false;
-            }
-            $this->args = array();
-            $this->remove = array(1);
-            
-            mon_log(get_class($t)." => ".__CLASS__);
-            return true;
-        } 
-        return false;
+        if ($t->getNext()->checkNotToken(T_CLOSE_TAG)) { return false; }
+        
+       if ($t->hasNext(1) && 
+           $t->getNext(1)->checkToken(T_INLINE_HTML)) {
+               // @note avoid polluting rawtext_regex 
+           return false;
+       }
+       $this->args = array();
+       $this->remove = array(1);
+       
+       mon_log(get_class($t)." => ".__CLASS__);
+       return true;
     }
 }
 
