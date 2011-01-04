fad2a3b7ac66c906a8f89f883eecc2f7b25ee088
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2011-01-04 14:19:20 +0100

    speed optimisation for ternary op

--- tests/tokenizeur/class.ternaryop.test.php
+++ tests/tokenizeur/class.ternaryop.test.php
@@ -1,25 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 - 2011 Alter Way Solutions (France)               |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */include_once('Analyseur_Framework_TestCase.php');
+include_once('Analyseur_Framework_TestCase.php');
 
 class Ternaryop_Test extends Analyseur_Framework_TestCase
 {
-    /* 26 methodes */
+    /* 29 methodes */
     public function testTernaryop1()  { $this->generic_test('ternaryop.1'); }
     public function testTernaryop2()  { $this->generic_test('ternaryop.2'); }
     public function testTernaryop3()  { $this->generic_test('ternaryop.3'); }
@@ -46,6 +30,9 @@ class Ternaryop_Test extends Analyseur_Framework_TestCase
     public function testTernaryop24()  { $this->generic_test('ternaryop.24'); }
     public function testTernaryop25()  { $this->generic_test('ternaryop.25'); }
     public function testTernaryop26()  { $this->generic_test('ternaryop.26'); }
+    public function testTernaryop27()  { $this->generic_test('ternaryop.27'); }
+    public function testTernaryop28()  { $this->generic_test('ternaryop.28'); }
+    public function testTernaryop29()  { $this->generic_test('ternaryop.29'); }
 
 }
 
