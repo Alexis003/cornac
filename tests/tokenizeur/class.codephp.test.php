3043491994163816bf5d08fe8ed7fd82583ab024
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2011-01-04 11:35:41 +0100

    support for short tags strangely parsed

--- tests/tokenizeur/class.codephp.test.php
+++ tests/tokenizeur/class.codephp.test.php
@@ -1,25 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 - 2011 Alter Way Solutions (France)               |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */include_once('Analyseur_Framework_TestCase.php');
+include_once('Analyseur_Framework_TestCase.php');
 
 class Codephp_Test extends Analyseur_Framework_TestCase
 {
-    /* 21 methodes */
+    /* 24 methodes */
     public function testCodephp1()  { $this->generic_test('codephp.1'); }
     public function testCodephp2()  { $this->generic_test('codephp.2'); }
     public function testCodephp3()  { $this->generic_test('codephp.3'); }
@@ -41,6 +25,9 @@ class Codephp_Test extends Analyseur_Framework_TestCase
     public function testCodephp19()  { $this->generic_test('codephp.19'); }
     public function testCodephp20()  { $this->generic_test('codephp.20'); }
     public function testCodephp21()  { $this->generic_test('codephp.21'); }
+    public function testCodephp22()  { $this->generic_test('codephp.22'); }
+    public function testCodephp23()  { $this->generic_test('codephp.23'); }
+    public function testCodephp24()  { $this->generic_test('codephp.24'); }
 
 }
 
