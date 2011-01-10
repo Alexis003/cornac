69f649e1adf87d375b3f06475cf3cda2b95ecf56
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-10 06:47:11 +0100

    merged 2 constraints

--- prepare/typehint_regex.php
+++ prepare/typehint_regex.php
@@ -39,10 +39,8 @@ class typehint_regex extends analyseur_regex {
         } elseif ($t->getPrev(1)->checkOperator('&') && 
                   $t->getPrev(2)->checkToken(T_FUNCTION)) {
             // @note this is a function
-        } elseif ($t->getNext( )->checkClass('Token') && 
-                  $t->getNext(1)->checkClass(array('variable','affectation'))) {
-            // @note this is a function
-        } elseif ($t->getNext( )->checkClass('_nsname') && 
+        } elseif ($t->hasNext(2) &&
+                  $t->getNext( )->checkClass(array('Token','_nsname')) && 
                   $t->getNext(1)->checkClass(array('variable','affectation'))) {
             // @note this is a function
         } else { 
