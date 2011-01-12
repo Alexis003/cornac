644d44070205fe7b530499a20d4cd94993a4833a
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-12 22:44:31 +0100

    fix tests

--- tests/tokenizeur/class.literals.test.php
+++ tests/tokenizeur/class.literals.test.php
@@ -1,26 +1,9 @@
 <?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 - 2011 Alter Way Solutions (France)               |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */
 include_once('Analyseur_Framework_TestCase.php');
 
 class Literals_Test extends Analyseur_Framework_TestCase
 {
-    /* 14 methodes */
+    /* 15 methodes */
     public function testLiterals1()  { $this->generic_test('literals.1'); }
     public function testLiterals2()  { $this->generic_test('literals.2'); }
     public function testLiterals3()  { $this->generic_test('literals.3'); }
@@ -35,6 +18,7 @@ class Literals_Test extends Analyseur_Framework_TestCase
     public function testLiterals12()  { $this->generic_test('literals.12'); }
     public function testLiterals13()  { $this->generic_test('literals.13'); }
     public function testLiterals14()  { $this->generic_test('literals.14'); }
+    public function testLiterals15()  { $this->generic_test('literals.15'); }
 
 }
 
