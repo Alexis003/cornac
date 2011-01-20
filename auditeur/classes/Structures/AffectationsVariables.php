b3e846fb1d69776bec5101c03fb1a83bf63f1ffa
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-20 17:39:21 +0100

    fix sql presentation, removed token_traite

--- auditeur/classes/Structures/AffectationsVariables.php
+++ auditeur/classes/Structures/AffectationsVariables.php
@@ -34,7 +34,8 @@ SELECT NULL, T1.file, T2.code, T1.id, '{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens> T2
     ON T1.file = T2.file AND T2.left = T1.left + 1
-WHERE T1.type = 'affectation'  AND T2.type = 'variable'
+WHERE T1.type = 'affectation'  AND 
+      T2.type = 'variable'
 SQL;
         $this->exec_query_insert('report', $query);    
 
@@ -43,20 +44,26 @@ SQL;
 SELECT NULL, T1.file, T3.code, T1.id,'{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens> T2
-    ON T1.file = T2.file AND T2.left = T1.left + 1
+    ON T1.file = T2.file AND
+       T2.left = T1.left + 1
 JOIN <tokens> T3
     ON T1.file = T3.file AND T3.left = T1.left + 2
-WHERE T1.type = 'affectation'  AND T2.type = '_array'
+WHERE T1.type = 'affectation'  AND
+      T2.type = '_array'
 SQL;
         $this->exec_query_insert('report', $query);    
 
 // @note property
         $query = <<<SQL
-SELECT NULL, T1.file, T2.code, T1.id,'{$this->name}', 0
+SELECT NULL, T1.file, TC.code, T1.id,'{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens> T2
-    ON T1.file = T2.file AND T2.left = T1.left + 1
-WHERE T1.type = 'affectation'  AND T2.type = 'property'
+    ON T1.file = T2.file AND 
+       T2.left = T1.left + 1
+JOIN tu_cache TC
+    ON TC.id = T2.id
+WHERE T1.type = 'affectation' AND
+      T2.type = 'property'
 SQL;
         $this->exec_query_insert('report', $query);    
 
@@ -65,12 +72,16 @@ SQL;
 SELECT NULL, T1.file, T3.code, T1.id,'{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens> T2
-    ON T1.file = T2.file AND T2.left = T1.left + 1
+    ON T1.file = T2.file AND 
+       T2.left = T1.left + 1
 JOIN <tokens_tags> TT
-    ON TT.token_id = T2.id AND TT.type = 'property'
+    ON TT.token_id = T2.id AND 
+       TT.type = 'property'
 JOIN <tokens> T3
-    ON T1.file = T3.file AND T3.id = TT.token_sub_id
-WHERE T1.type = 'affectation'  AND T2.type = 'property_static'
+    ON T1.file = T3.file AND 
+       T3.id = TT.token_sub_id
+WHERE T1.type = 'affectation'  AND 
+      T2.type = 'property_static'
 SQL;
         $this->exec_query_insert('report', $query);    
 
@@ -79,11 +90,18 @@ SQL;
 SELECT NULL, T1.file, T4.code, T1.id,'{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens> T2
-    ON T1.file = T2.file AND T2.left = T1.left + 1 AND T2.type = 'functioncall'
+    ON T1.file = T2.file     AND 
+       T2.left = T1.left + 1 AND
+       T2.type = 'functioncall'
 JOIN <tokens> T3
-    ON T1.file = T3.file AND T3.left = T1.left + 2 AND T3.type = 'token_traite' AND T3.code = 'list'
+    ON T1.file = T3.file AND
+       T3.left = T1.left + 2 AND
+       T3.type = '_functionname_' AND
+       T3.code = 'list'
 JOIN <tokens> T4
-    ON T1.file = T4.file AND T4.left BETWEEN T2.left AND T2.right AND T4.type = 'variable'
+    ON T1.file = T4.file AND
+       T4.left BETWEEN T2.left AND T2.right AND
+       T4.type = 'variable'
 WHERE T1.type = 'affectation' 
 SQL;
         $this->exec_query_insert('report', $query);    
@@ -93,9 +111,11 @@ SQL;
 SELECT NULL, T1.file, T2.code, T1.id,'{$this->name}', 0
 FROM <tokens> T1
 JOIN <tokens_tags> TT1
-    ON TT1.token_id = T1.id AND TT1.type IN ('value','key')
+    ON TT1.token_id = T1.id AND 
+       TT1.type IN ('value','key')
 JOIN <tokens> T2
-    ON T1.file = T2.file AND TT1.token_sub_id = T2.id
+    ON T1.file = T2.file AND 
+       TT1.token_sub_id = T2.id
 WHERE T1.type = '_foreach'
 SQL;
         $this->exec_query_insert('report', $query);    
