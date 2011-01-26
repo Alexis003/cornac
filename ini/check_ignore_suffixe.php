02ee811c2efa80d9a47f01f913022d859a3e4804
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-26 19:02:29 +0100

    new extension, plus check in database

--- ini/check_ignore_suffixe.php
+++ ini/check_ignore_suffixe.php
@@ -16,7 +16,7 @@
    | Author: Damien Seguy <damien.seguy@gmail.com>                        |
    +----------------------------------------------------------------------+
  */
-$list = ".gif,.jpg,.bak,.psd,.png,.htm,.xml,.js,.txt,.old,.gz,.db,.jpeg,.html,.swf,.scc,.rar,.zip,.ico,.sh,.patch,.sql,.dll,.ai,.afm,.jar,.docx,.dat,.mp3,.ttf,.table,.pdf,.z,.fla,.bmp,.cgi,.csv,.xsl,svg,.doc,.docx,.odt,.odp,.wsdl,.xsd,.dist,.markdown,.awk,.war,.md,.ott,.odg,.ts,.xmi,.dba,.ezt,.o,.cpp,.java,.manifest,.rng,.md5,.jsb,.in,.dia,.ctp,.sgml,.sample,.mxml,.dtd,.lin,.fre,.JPG,.tar,.xslt,.flv,.resx,.mpg,.info,.tiff,.exe,.rdf,.rss,.yml,.bat,.py,.pl,.c,.h,.pfb,.xap,.xul,.css,.config,.darwin,.default,.hlp,.pas,.bdsproj,.bdsgroup,.bpr,.DLL,.src,.CAB,.xcf,.woff,.eot,.icc,.xad,.xls,.doc,.ppt,.so,.sxw,.sit,.sitx,.desc,.tex,.linux,.mo,.po,.tif,.tga,.pcx,.lss,.bpk,.sxd,.bin,.as3proj,.selenium,.mwb,.vsd";
+$list = ".gif,.jpg,.bak,.psd,.png,.htm,.xml,.js,.txt,.old,.gz,.db,.jpeg,.html,.swf,.scc,.rar,.zip,.ico,.sh,.patch,.sql,.dll,.ai,.afm,.jar,.docx,.dat,.mp3,.ttf,.table,.pdf,.z,.fla,.bmp,.cgi,.csv,.xsl,.svg,.doc,.docx,.odt,.odp,.wsdl,.xsd,.dist,.markdown,.awk,.war,.md,.ott,.odg,.ts,.xmi,.dba,.ezt,.o,.cpp,.java,.manifest,.rng,.md5,.jsb,.in,.dia,.ctp,.sgml,.sample,.mxml,.dtd,.lin,.fre,.JPG,.tar,.xslt,.flv,.resx,.mpg,.info,.tiff,.exe,.rdf,.rss,.yml,.bat,.py,.pl,.c,.h,.pfb,.xap,.xul,.css,.config,.darwin,.default,.hlp,.pas,.bdsproj,.bdsgroup,.bpr,.DLL,.src,.CAB,.xcf,.woff,.eot,.icc,.xad,.xls,.doc,.ppt,.so,.sxw,.sit,.sitx,.desc,.tex,.linux,.mo,.po,.tif,.tga,.pcx,.lss,.bpk,.sxd,.bin,.as3proj,.selenium,.mwb,.vsd,.conf,.mso";
 $list = explode(',', $list);
 $list2 = array_unique($list);
 
@@ -29,16 +29,33 @@ include('../libs/write_ini_file.php');
 
 $files = glob('./*.ini');
 
+include('../libs/database.php');
+
 foreach($files as $file) {
     $ini = parse_ini_file($file, true);
     if ($ini['tokenizeur']['ignore_suffixe'] != $list) {
         $ini['tokenizeur']['ignore_suffixe'] = $list;
         write_ini_file($ini, $file);
+
     
         print "$file : updated\n";
     } else {
         print "$file : ok\n";
     }
+    $INI = $ini;
+    
+    $DATABASE = new database();
+    $query = "SHOW TABLES LIKE \"<tasks>\"";
+    $res = $DATABASE->query($query);
+    
+    if ($res->rowCount() == 1) {
+        $query = "DELETE FROM <tasks> WHERE RIGHT(target, LOCATE('.', REVERSE(target))) IN ('".join("','", $list2)."')";
+        $res = $DATABASE->query($query);
+        if ($res->rowCount() > 0) {
+            print $res->rowCount()." tasks removed\n";
+        }
+    }
+    
+    unset($DATABASE);
 }
-
 ?>
