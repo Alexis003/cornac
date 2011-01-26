07e477526537f14d8bfe89bc838a8e40b79ef47a
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-26 18:47:02 +0100

    removed useless comment

--- auditeur/classes/Quality/ClassesNotInSameFile.php
+++ auditeur/classes/Quality/ClassesNotInSameFile.php
@@ -29,7 +29,6 @@ class Quality_ClassesNotInSameFile extends modules {
 	public function analyse() {
         $this->clean_report();
 
-// @todo of course, update this useless query. :)
 	    $query = <<<SQL
 SELECT NULL, T1.file, T2.code, T1.id, '{$this->name}', 0
 FROM <tokens> T1 
