3c0f790912947529f00e6e95c6b686bcbd9ccdab
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-26 18:49:17 +0100

    removed dependencies

--- auditeur/classes/Structures/CaseWithoutBreak.php
+++ auditeur/classes/Structures/CaseWithoutBreak.php
@@ -26,15 +26,9 @@ class Structures_CaseWithoutBreak extends modules {
         parent::__construct($mid);
 	}
 
-// @doc if this analyzer is based on previous result, use this to make sure the results are here
-	function dependsOn() {
-	    return array();
-	}
-
 	public function analyse() {
         $this->clean_report();
 
-// @todo of course, update this useless query. :)
 	    $query = <<<SQL
 SELECT NULL, T1.file, TC.code, T1.id, '{$this->name}', 0
 FROM <tokens> T1
