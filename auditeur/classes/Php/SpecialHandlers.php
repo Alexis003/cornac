50384fd030364ae388e756c27cfc1cff4922f366
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-26 18:48:12 +0100

    fixed dependencies

---
+++ auditeur/classes/Php/SpecialHandlers.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+class Php_SpecialHandlers extends modules {
+	protected	$title = 'Php special handlers';
+	protected	$description = 'Spot usage of PHP special handlers, such as session handler, error handler, etc.';
+
+	function __construct($mid) {
+        parent::__construct($mid);
+	}
+
+	function dependsOn() {
+	    return array('Functions_Php');
+	}
+
+	public function analyse() {
+        $this->clean_report();
+
+        $in = modules::getPHPHandlers();
+        $this->in = '"'.join('", "', $in).'"';
+
+	    $query = <<<SQL
+SELECT NULL, T1.file, T1.element, T1.id, '{$this->name}', 0
+FROM <report> T1
+WHERE T1.module = 'Functions_Php' AND
+      T1.element IN ({$this->in})
+SQL;
+        $this->exec_query_insert('report', $query);
+
+        return true;
+	}
+}
+
+?>
