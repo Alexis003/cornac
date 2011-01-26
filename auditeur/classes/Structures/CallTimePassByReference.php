d37d5a37ac5d6724c831495217c12ae25f792f25
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-26 18:55:53 +0100

    new analyzer

--- auditeur/classes/Structures/CallTimePassByReference.php
+++ auditeur/classes/Structures/CallTimePassByReference.php
@@ -2,26 +2,31 @@
 
 
 class Structures_CallTimePassByReference extends modules {
-	protected	$title = 'Title for Structures_CallTimePassByReference';
-	protected	$description = 'This is the special analyzer Structures_CallTimePassByReference (default doc).';
+	protected	$title = 'Call-time pass-by-reference';
+	protected	$description = 'Spot all function/method call that force pass by reference at call time.';
 
 	function __construct($mid) {
         parent::__construct($mid);
 	}
 
-// @doc if this analyzer is based on previous result, use this to make sure the results are here
-	function dependsOn() {
-	    return array();
-	}
-
 	public function analyse() {
         $this->clean_report();
 
-// @todo of course, update this useless query. :)
 	    $query = <<<SQL
-SELECT NULL, T1.file, T1.code, T1.id, '{$this->name}', 0
-    FROM <tokens> T1
-    WHERE type = 'variable'
+SELECT NULL, T1.file, T3.code, T1.id, '{$this->name}', 0
+FROM <tokens> T1
+JOIN <tokens> T2
+    ON T2.file = T1.file AND
+       T2.left BETWEEN T1.left AND T1.right AND
+       T2.level = T1.level + 1 AND
+       T2.type = 'reference'
+JOIN <tokens_tags> TT
+    ON TT.token_sub_id = T1.id
+JOIN <tokens> T3
+    ON TT.token_id = T3.id AND
+       T3.file = T1.file AND
+       T3.type = 'functioncall'
+WHERE T1.type = 'arglist'
 SQL;
         $this->exec_query_insert('report', $query);
 
