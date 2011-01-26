c97ca354a9c51044014997d110873dff3f106b6f
Seguy Damien <alterway@AlterWayConsulting.local>
2011-01-26 18:48:41 +0100

    fixed SQL pres, removed dependencies

--- auditeur/classes/Quality/ConstructNameOfClass.php
+++ auditeur/classes/Quality/ConstructNameOfClass.php
@@ -26,21 +26,15 @@ class Quality_ConstructNameOfClass extends modules {
         parent::__construct($mid);
 	}
 
-// @doc if this analyzer is based on previous result, use this to make sure the results are here
-	function dependsOn() {
-	    return array();
-	}
-
 	public function analyse() {
         $this->clean_report();
 
-// @todo of course, update this useless query. :)
 	    $query = <<<SQL
 SELECT NULL, T1.file, concat(class, '::', T1.code), T1.id, '{$this->name}', 0
 FROM <tokens> T1
-    WHERE type = '_function' AND
-          (code = class        OR
-           code = '__construct'  )
+WHERE type = '_function' AND
+      (code = class        OR
+       code = '__construct'  )
 SQL;
         $this->exec_query_insert('report', $query);
 
