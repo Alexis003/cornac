e850f6561de2c7bdbe66d31fa063a1406952d934
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2011-02-02 17:30:43 +0100

    created external structures / libraries

--- auditeur/classes/Quality/ExternalLibraries.php
+++ auditeur/classes/Quality/ExternalLibraries.php
@@ -1,35 +1,21 @@
-<?php
-/*
-   +----------------------------------------------------------------------+
-   | Cornac, PHP code inventory                                           |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010 - 2011 Alter Way Solutions (France)               |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Damien Seguy <damien.seguy@gmail.com>                        |
-   +----------------------------------------------------------------------+
- */
+<?php 
+
 
 class Quality_ExternalLibraries extends modules {
-	protected	$title = 'Common libraries';
-	protected	$description = 'Spot commonly used libraries.';
+	protected	$title = 'Title for Quality_ExternalStructures';
+	protected	$description = 'This is the special analyzer Quality_ExternalStructures (default doc).';
 
 	function __construct($mid) {
         parent::__construct($mid);
 	}
 
+// @doc if this analyzer is based on previous result, use this to make sure the results are here
+	function dependsOn() {
+	    return array('Quality_ExternalStructures');
+	}
+
 	public function analyse() {
         $this->clean_report();
-// @todo use also constantes
-// @todo use also functions
-// @todo spot versions also ? 
 
         $list = modules::getPopLib();
         
@@ -39,23 +25,10 @@ class Quality_ExternalLibraries extends modules {
             // @doc search for usage as class extensions
             $query = <<<SQL
 SELECT NULL, T1.file, '$ext', T1.id, '{$this->name}', 0
-FROM <tokens> T1
-JOIN <tokens_tags> TT 
-    ON TT.token_id = T1.id AND
-       TT.type = 'extends'
-JOIN <tokens> T2
-    ON TT.token_sub_id = T2.id AND
-       T1.file = T2.file AND 
-       T2.code IN ($in)
-WHERE T1.type='_class'
-SQL;
-            $this->exec_query_insert('report', $query);
-
-            // @doc search for usage as instanciation
-            $query = <<<SQL
-SELECT NULL, TR.file, '$ext', TR.id, '{$this->name}', 0
-FROM <report> TR
-WHERE TR.element IN ($in)
+FROM <report> T1
+WHERE T1.module = 'Quality_ExternalStructures' AND
+      T1.element IN ($in)
+GROUP BY '$ext'
 SQL;
             $this->exec_query_insert('report', $query);
         }
