ae25485bdc5b5070725a5ecda56a662368729dce
Seguy Damien <alterway@AlterWayConsulting.local>
2011-02-03 19:18:36 +0100

    function that will request HTTP pages

---
+++ auditeur/classes/Ext/HttpBrowse.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+class Ext_HttpBrowse extends functioncalls {
+	protected	$title = 'HTTP requests';
+	protected	$description = 'This is the special analyzer Ext_HttpBrowse (default doc).';
+
+	function __construct($mid) {
+        parent::__construct($mid);
+	}
+
+	public function analyse() {
+	    $this->functions = array('file_get_contents','curl_exec','fopen');
+	    
+	    parent::analyse();
+	    
+	    return true;
+	}
+}
+
+?>
