0be86b7cb4531aa260edb6c4f59bd41d041d18c3
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2011-02-11 11:24:13 +0100

    fixed static handling

--- tests/tokenizeur/class.static.test.php
+++ tests/tokenizeur/class.static.test.php
@@ -3,7 +3,7 @@ include_once('Analyseur_Framework_TestCase.php');
 
 class Static_Test extends Analyseur_Framework_TestCase
 {
-    /* 7 methodes */
+    /* 8 methodes */
     public function testStatic1()  { $this->generic_test('static.1'); }
     public function testStatic2()  { $this->generic_test('static.2'); }
     public function testStatic3()  { $this->generic_test('static.3'); }
@@ -11,6 +11,7 @@ class Static_Test extends Analyseur_Framework_TestCase
     public function testStatic5()  { $this->generic_test('static.5'); }
     public function testStatic6()  { $this->generic_test('static.6'); }
     public function testStatic7()  { $this->generic_test('static.7'); }
+    public function testStatic8()  { $this->generic_test('static.8'); }
 
 }
 
---
+++ tests/tokenizeur/scripts/static.8.test.php
@@ -0,0 +1,11 @@
+<?php
+    static $a;
+    static $b,$c;
+    static $d,$e,$f;
+    static $g = array();
+    static $g = array(), $h = array();
+    static $i = array(), $j = array(), $k = array();
+    static $l = array(), $m, $k = array();
+    static $recursion, $scanned_files = array();
+
+?>
