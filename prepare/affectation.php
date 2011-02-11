be920502a3bf611e1c3df469c50201c47dd5d126
Seguy Damien <alterway@alterwayconsulting.paris.aws>
2011-02-11 11:25:02 +0100

    removed useless code

--- prepare/affectation.php
+++ prepare/affectation.php
@@ -18,8 +18,6 @@
  */
 
 class affectation extends instruction {
-    protected $_static        = null;
-    protected $_visibility    = null;
     protected $left           = null;
     protected $operator       = null;
     protected $right          = null;
@@ -27,30 +25,15 @@ class affectation extends instruction {
     function __construct($expression) {
         parent::__construct(array());
         
-        if (is_array($expression)) {
-            while ($expression[0]->checkToken(array(T_PUBLIC, T_PRIVATE, T_PROTECTED, T_STATIC))) {
-                if ($expression[0]->checkToken(array(T_PUBLIC, T_PRIVATE, T_PROTECTED))) {
-                    $this->_visibility = $this->makeProcessedToken('_ppp_', $expression[0]);
-                } elseif ($expression[0]->checkToken(T_STATIC)) {
-                    $this->_static = $this->makeProcessedToken('_static_', $expression[0]);
-                }
-
-                unset($expression[0]);
-                $expression = array_values($expression);
-            }
-
-            if (count($expression) != 3) {
-                $this->stopOnError("Affectation with unexpected number of valudes : ".count($expression)." received\n");
-            }
-
-            $this->left = $expression[0];
-            $expression[1]->setLine($expression[0]->getLine());
-            $this->operator = $this->makeProcessedToken('_affectation_', $expression[1]);
-            $this->right = $expression[2];
-            $this->setLine($this->left->getLine());
-        } else {
-            $this->stopOnError("Affectation received strange number of values : ".count($expression)." received\n");
+        if (count($expression) != 3) {
+            $this->stopOnError("Affectation with unexpected number of values : ".count($expression)." received\n");
         }
+
+        $this->left = $expression[0];
+        $expression[1]->setLine($expression[0]->getLine());
+        $this->operator = $this->makeProcessedToken('_affectation_', $expression[1]);
+        $this->right = $expression[2];
+        $this->setLine($this->left->getLine());
     }
 
     function __toString() {
@@ -67,14 +50,6 @@ class affectation extends instruction {
 
     function getRight() {
         return $this->right;
-    }
-
-    function getVisibility() {
-        return $this->_visibility;
-    }
-
-    function getStatic() {
-        return $this->_static;
     }
 
     function getToken() {
