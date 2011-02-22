823aa5542394333276ec9ab32b51cfe281cdeee3
Seguy Damien <alterway@AlterWayConsulting.local>
2011-02-22 15:34:50 +0100

    useless now

--- tests/auditeur/test.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-./tokenizeur.php -r -d ./tests/auditeur/scripts/ -g mysql,cache -I testsunitaires
-cd auditeur
-php auditeur.php tu
-php lecture_module.php -I testsunitaires -a appelsfonctions -f ./tests/auditeur/scripts/appelsfonctions.php
-
